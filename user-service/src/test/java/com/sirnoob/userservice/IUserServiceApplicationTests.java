baa19c90d6bd61be246a46cd1498a93117c69c9b
Martin <martinald467@gmail.com>
2020-08-28 00:10:00 +0200

    creado test del repositorio de los usuarios

---
+++ user-service/src/test/java/com/sirnoob/userservice/IUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirnoob.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
