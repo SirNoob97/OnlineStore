004bf10f4c4382447f53ddb58fe8be762c17a065
Martin <martinald467@gmail.com>
2020-08-29 23:43:57 +0200

    configuracion para connecion a base de datos y archivo para inicializarla para el testeo
    ya que la forma de coneccion es tan distinta debo investigar un poco mas la conecion con h2 y ver como persistir mi data.sql para testeo al momento de iniciar la app

---
+++ product-service/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO categories (category_id, category_name) VALUES (1, 'Computer');
+INSERT INTO categories (category_id, category_name) VALUES (2, 'CellPhone');
+INSERT INTO categories (category_id, category_name) VALUES (3, 'Camera');
+
+
+INSERT INTO products (product_id, product_number, product_name, product_price, product_description, product_stock, create_at, category_id)
+VALUES (1, 9632587, 'Iphone X', 1156.60, 'Apple IOS', 15, '01-01-2001', 2);
+
+INSERT INTO products (product_id, product_number, product_name, product_price, product_description, product_stock, create_at, category_id)
+VALUES (2, 1232587, 'Toshiba', 750.00, 'Core i7', 10, '01-01-2006', 1);
+
+INSERT INTO products (product_id, product_number, product_name, product_price, product_description, product_stock, create_at, category_id)
+VALUES (3, 9632512, 'Samsung', 300.45, 'Personal Camera', 20, '01-01-2010', 3);
+
+INSERT INTO sub_categories (sub_category_id, sub_category_name, category_id) VALUES (1, 'Laptop', 1);
+INSERT INTO sub_categories (sub_category_id, sub_category_name, category_id) VALUES (2, '1600 Mpx', 3);
+INSERT INTO sub_categories (sub_category_id, sub_category_name, category_id) VALUES (3, 'Iphone', 2)
+
+
+INSERT INTO sub_categories_products (sub_category_id, product_id) SELECT sub_categories.sub_category_id, products.product_id 
+FROM sub_categories inner join products ON sub_categories.sub_category_id = 1 AND products.products_id = 2;
+
+INSERT INTO sub_categories_products (sub_category_id, product_id) SELECT sub_categories.sub_category_id, products.product_id 
+FROM sub_categories inner join products ON sub_categories.sub_category_id = 2 AND products.products_id = 3;
+
+INSERT INTO sub_categories_products (sub_category_id, product_id) SELECT sub_categories.sub_category_id, products.product_id 
+FROM sub_categories inner join products ON sub_categories.sub_category_id = 3 AND products.products_id = 1;
