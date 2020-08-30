504e1c1ec082ef8a08d07436386fd959cafd307e
Martin <martinald467@gmail.com>
2020-08-30 22:33:27 +0200

    implementada la logica para obtener todos los productos junto con su categoria despues de que termine los demas metodos movere los mapeos a una clase a parte
    dependiendo de lo que investigue del problema con las subcategorias el metodo de obtener todos los productos se quedara asi

--- product-service/src/main/java/com/sirnoob/productservice/service/ProductServicesImpl.java
+++ product-service/src/main/java/com/sirnoob/productservice/service/ProductServicesImpl.java
@@ -1,19 +1,28 @@
 package com.sirnoob.productservice.service;
 
-import org.springframework.stereotype.Service;
-
+import com.sirnoob.productservice.entity.Category;
 import com.sirnoob.productservice.entity.Product;
+import com.sirnoob.productservice.entity.SubCategory;
 import com.sirnoob.productservice.repository.IProductRespository;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
-public class ProductServicesImpl implements IProductService{
+public class ProductServicesImpl implements IProductService {
 
 	private final IProductRespository iProductRespository;
+	private final DatabaseClient databaseClient;
 
 	@Override
 	public Mono<Product> getProduct(Long productId) {
@@ -37,7 +46,26 @@ public class ProductServicesImpl implements IProductService{
 
 	@Override
 	public Flux<Product> listAll() {
-		return iProductRespository.findAll();
+		final String query = "SELECT * FROM products INNER JOIN categories ON products.category_id = categories.category_id;";
+
+		return databaseClient.execute(query).map(productDB -> {
+
+			Long productId = productDB.get("product_id", Long.class);
+
+			Product product = Product.builder().productId(productId)
+					.productNumber(productDB.get("product_number", Integer.class))
+					.productName(productDB.get("product_name", String.class))
+					.productDescription(productDB.get("product_description", String.class))
+					.productPrice(productDB.get("product_price", Double.class))
+					.productStock(productDB.get("product_stock", Integer.class))
+					.createAt(LocalDate.from(productDB.get("create_at", LocalDate.class)))
+					.category(Category.builder().categoryId(productDB.get("category_id", Long.class))
+							.categoryName(productDB.get("category_name", String.class)).build())
+					.build();
+			return product;
+		}).all();
+
+	
 	}
 
 	@Override
@@ -47,7 +75,7 @@ public class ProductServicesImpl implements IProductService{
 
 	@Override
 	public Flux<Product> findBySubCategoryId(Long categoryId) {
-		return iProductRespository.findBySubCategoriesSubCategoryId(categoryId);
+		return null;// iProductRespository.findBySubCategorySubCategoryId(categoryId);
 	}
 
 	@Override
