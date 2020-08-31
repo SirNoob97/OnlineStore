fc34c6da2e9ae0781a5c72eaec8338c3dbc58b5e
Martin <martinald467@gmail.com>
2020-08-31 23:22:58 +0200

    cambio del tipo de retorno a dto de producto

--- product-service/src/main/java/com/sirnoob/productservice/service/IProductService.java
+++ product-service/src/main/java/com/sirnoob/productservice/service/IProductService.java
@@ -1,5 +1,6 @@
 package com.sirnoob.productservice.service;
 
+import com.sirnoob.productservice.dto.template.ProductResponse;
 import com.sirnoob.productservice.entity.Product;
 
 import reactor.core.publisher.Flux;
@@ -8,18 +9,17 @@ import reactor.core.publisher.Mono;
 public interface IProductService {
 
 	public Mono<Product> getProduct(Long productId);
-	
-	public Mono<Product> createProduct(Product product);
-	
-	public Mono<Product> updateProduct(Product product);
-	
-	public Mono<Product> deleteProduct(Long productId);
-	
-	public Flux<Product> listAll();
 
-	public Flux<Product> findByCategoryId(Long categoryId);
-	
-	public Flux<Product> findBySubCategoryId(Long categoryId);
-	
-	public Flux<Product> findByProductNumber(Integer productNumber);
+	public Mono<Product> createProduct(Product product);
+
+	public Mono<Product> updateProduct(Product product);
+
+	public Mono<Product> deleteProduct(Long productId);
+
+	public Flux<ProductResponse> listAll();
+
+	public Flux<ProductResponse> findByMainCategory(Long categoryId);
+
+	public Mono<ProductResponse> getProductByProductNumber(Integer productNumber);
+
 }
