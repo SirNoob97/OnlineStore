f2b40101e41ef22492b760d0193e3a5ffc392782
Martin <martinald467@gmail.com>
2020-08-31 23:24:37 +0200

    implementados los metodos para obtener productos por su numero(no id) y por categoria tambien usado mappeo para evitar que el servicio conozca a la entidad de los productos --pendiente

--- product-service/src/main/java/com/sirnoob/productservice/service/ProductServicesImpl.java
+++ product-service/src/main/java/com/sirnoob/productservice/service/ProductServicesImpl.java
@@ -1,32 +1,26 @@
 package com.sirnoob.productservice.service;
 
-import com.sirnoob.productservice.entity.Category;
-import com.sirnoob.productservice.entity.Product;
-import com.sirnoob.productservice.entity.SubCategory;
-import com.sirnoob.productservice.repository.IProductRespository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
+
+import com.sirnoob.productservice.dto.mapper.IProductMapper;
+import com.sirnoob.productservice.dto.template.ProductResponse;
+import com.sirnoob.productservice.entity.Product;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ProductServicesImpl implements IProductService {
 
-	private final IProductRespository iProductRespository;
+	private final IProductMapper iProductMapper;
 	private final DatabaseClient databaseClient;
 
 	@Override
 	public Mono<Product> getProduct(Long productId) {
-		return iProductRespository.findById(productId);
+		return null;
 	}
 
 	@Override
@@ -45,42 +39,21 @@ public class ProductServicesImpl implements IProductService {
 	}
 
 	@Override
-	public Flux<Product> listAll() {
-		final String query = "SELECT * FROM products INNER JOIN categories ON products.category_id = categories.category_id;";
-
-		return databaseClient.execute(query).map(productDB -> {
-
-			Long productId = productDB.get("product_id", Long.class);
-
-			Product product = Product.builder().productId(productId)
-					.productNumber(productDB.get("product_number", Integer.class))
-					.productName(productDB.get("product_name", String.class))
-					.productDescription(productDB.get("product_description", String.class))
-					.productPrice(productDB.get("product_price", Double.class))
-					.productStock(productDB.get("product_stock", Integer.class))
-					.createAt(LocalDate.from(productDB.get("create_at", LocalDate.class)))
-					.category(Category.builder().categoryId(productDB.get("category_id", Long.class))
-							.categoryName(productDB.get("category_name", String.class)).build())
-					.build();
-			return product;
-		}).all();
-
-	
+	public Flux<ProductResponse> listAll() {
+		final String query = "SELECT * FROM products INNER JOIN main_categories ON products.category_id = main_categories.category_id";
+		return databaseClient.execute(query).map(iProductMapper::mapToProductResponse).all();
 	}
 
 	@Override
-	public Flux<Product> findByCategoryId(Long categoryId) {
-		return iProductRespository.findByCategoryCategoryId(categoryId);
+	public Flux<ProductResponse> findByMainCategory(Long categoryId) {
+		final String query = "SELECT * FROM products INNER JOIN main_categories ON main_categories.category_id = products.category_id WHERE products.category_id = :categoryId";
+		return databaseClient.execute(query).bind("categoryId", categoryId).map(iProductMapper::mapToProductResponse).all();
 	}
 
 	@Override
-	public Flux<Product> findBySubCategoryId(Long categoryId) {
-		return null;// iProductRespository.findBySubCategorySubCategoryId(categoryId);
-	}
-
-	@Override
-	public Flux<Product> findByProductNumber(Integer productNumber) {
-		return iProductRespository.findByProductNumber(productNumber);
+	public Mono<ProductResponse> getProductByProductNumber(Integer productNumber) {
+		final String query = "SELECT * FROM products JOIN main_categories ON products.category_id = main_categories.category_id WHERE products.product_number = :productNumber";
+		return databaseClient.execute(query).bind("productNumber", productNumber).map(iProductMapper::mapToProductResponse).one();
 	}
 
 }
