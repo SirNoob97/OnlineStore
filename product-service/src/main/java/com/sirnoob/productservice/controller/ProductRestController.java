c1b1c5bee336747cf7175d95fb0247a91ae750ea
Martin <martinald467@gmail.com>
2020-08-31 23:18:59 +0200

    agregados metodo para obtener productos por su numero(no el id, este lo dejare  solo para usarlo en la DB) y por su categoria

--- product-service/src/main/java/com/sirnoob/productservice/controller/ProductRestController.java
+++ product-service/src/main/java/com/sirnoob/productservice/controller/ProductRestController.java
@@ -2,14 +2,16 @@ package com.sirnoob.productservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sirnoob.productservice.entity.Product;
+import com.sirnoob.productservice.dto.template.ProductResponse;
 import com.sirnoob.productservice.service.IProductService;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,7 +21,17 @@ public class ProductRestController {
 	private final IProductService iProductService;
 	
 	@GetMapping
-	public ResponseEntity<Flux<Product>> listAll(){
+	public ResponseEntity<Flux<ProductResponse>> listAll(){
 		return ResponseEntity.ok().body(iProductService.listAll());
+	}
+	
+	@GetMapping("/{product_number}")
+	public ResponseEntity<Mono<ProductResponse>> findByProductNumber(@PathVariable("product_number") Integer poductNumber){
+		return ResponseEntity.ok().body(iProductService.getProductByProductNumber(poductNumber));
+	}
+	
+	@GetMapping("/category/{id}")
+	public ResponseEntity<Flux<ProductResponse>> findByCategory(@PathVariable("id") Long id){
+		return ResponseEntity.ok().body(iProductService.findByMainCategory(id));
 	}
 }
