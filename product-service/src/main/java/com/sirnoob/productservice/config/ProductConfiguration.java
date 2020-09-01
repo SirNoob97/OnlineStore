0f10f12a7cb56157b7648bc51e916a1d0e042ce8
Martin <martinald467@gmail.com>
2020-09-01 18:49:59 +0200

    agregado metodo para obtener un bean de transactional operator y asi poder ejecutar multiples consultas en un mismo flujo

--- product-service/src/main/java/com/sirnoob/productservice/config/ProductConfiguration.java
+++ product-service/src/main/java/com/sirnoob/productservice/config/ProductConfiguration.java
@@ -3,7 +3,9 @@ package com.sirnoob.productservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
@@ -28,4 +30,10 @@ public class ProductConfiguration extends AbstractR2dbcConfiguration {
 	public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {		
 		return DatabaseClient.create(connectionFactory);
 	}
+	
+	@Bean
+	public TransactionalOperator transactionalOperator(ConnectionFactory connectionFactory) {
+		return TransactionalOperator.create(new R2dbcTransactionManager(connectionFactory));
+	}
+	
 }
