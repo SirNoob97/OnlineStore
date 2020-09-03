c5f9fb0b6f5f349e60d003ab3e226fafab0303fe
Martin <martinald467@gmail.com>
2020-09-03 18:17:42 +0200

    cambio de tipo y nombre para el codigo de barra de los productos en las repuestas para facturas

--- product-service/src/main/java/com/sirnoob/productservice/dto/ProductInvoiceResponse.java
+++ product-service/src/main/java/com/sirnoob/productservice/dto/ProductInvoiceResponse.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class ProductInvoiceResponse {
 
-	private Integer productNumber;
+	private Long productBarCode;
 	private String productName;
 	private String productDescription;
 	private Double productPrice;
