1bb0fb355154c15b1b68ef10a4eff68911ca2863
Martin <martinald467@gmail.com>
2020-09-03 21:44:23 +0200

    agregada una dependencia que puede que use para los test de la app

--- product-service/src/main/java/com/sirnoob/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/sirnoob/productservice/ProductServiceApplication.java
@@ -3,9 +3,15 @@ package com.sirnoob.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
+//	static {
+//		BlockHound.install();
+//	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
