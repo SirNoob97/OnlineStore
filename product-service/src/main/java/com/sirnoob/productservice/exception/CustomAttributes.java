084b55c260178043616aed0c1299b0be2b829d3d
Martin <martinald467@gmail.com>
2020-09-04 21:55:38 +0200

    agregada logica para mandar los mensajes de las validaciones --pendiente de mejorar

--- product-service/src/main/java/com/sirnoob/productservice/exception/CustomAttributes.java
+++ product-service/src/main/java/com/sirnoob/productservice/exception/CustomAttributes.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,9 +17,16 @@ public class CustomAttributes extends DefaultErrorAttributes{
 		Map<String, Object> errorsAttributes = super.getErrorAttributes(request, options);
 		Throwable throwable = getError(request);
 		
+		if (throwable instanceof WebExchangeBindException) {
+			ResponseStatusException responseStatusException = (ResponseStatusException) throwable;
+			errorsAttributes.put("message", findValidationDefaultMessage(responseStatusException.getMessage()));
+			
+			return errorsAttributes;
+		}
+		
 		if (throwable instanceof ResponseStatusException) {
 			ResponseStatusException responseStatusException = (ResponseStatusException) throwable;
-			errorsAttributes.put("message", responseStatusException.getMessage());
+			errorsAttributes.put("message",  responseStatusException.getMessage());
 			
 			return errorsAttributes;
 		}
@@ -26,5 +34,13 @@ public class CustomAttributes extends DefaultErrorAttributes{
 		return errorsAttributes;
 	}
 	
-
+//	He usado este metodo para mostrar el mensaje de error en las validaciones cuando hay un mal formato en los datos al crear y actualizar
+//	ya que no puedo obtener un binding result de las validaciones dejare esto asi hasta que averigue como manejarlas sin esa interface
+	
+	private String findValidationDefaultMessage(String message) {
+		int begin = message.lastIndexOf("default message [");
+		int end = message.lastIndexOf("]]");
+		
+		return message.substring((begin + 17), (end));
+	}
 }
