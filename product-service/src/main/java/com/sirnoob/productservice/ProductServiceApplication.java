5b049ae1d1590ff5da17e49f1bd961b44aa61931
Martin <martinald467@gmail.com>
2020-09-08 16:02:34 +0200

    reemplazo del micro servicio anterior usare ese projecto para experimentar con reactive web y r2bdc
    este projecto nuevo usare lo de siempre jpa y spring web

--- product-service/src/main/java/com/sirnoob/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/sirnoob/productservice/ProductServiceApplication.java
@@ -3,15 +3,9 @@ package com.sirnoob.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class ProductServiceApplication {
 
-//	static {
-//		BlockHound.install();
-//	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
