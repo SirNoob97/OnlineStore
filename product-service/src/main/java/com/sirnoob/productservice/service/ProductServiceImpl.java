d3c2763d9e31cf7a0a2de146ca0468db55c6fe03
Martin <martinald467@gmail.com>
2020-09-09 21:38:29 +0200

    implementados metodos de creacion y actulizacion de productos --pendientes

--- product-service/src/main/java/com/sirnoob/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/sirnoob/productservice/service/ProductServiceImpl.java
@@ -1,16 +1,24 @@
 package com.sirnoob.productservice.service;
 
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.sirnoob.productservice.dto.ProductInvoiceResponse;
 import com.sirnoob.productservice.dto.ProductRequest;
 import com.sirnoob.productservice.dto.ProductResponse;
 import com.sirnoob.productservice.entity.MainCategory;
+import com.sirnoob.productservice.entity.Product;
+import com.sirnoob.productservice.entity.SubCategory;
+import com.sirnoob.productservice.mapper.IProductMapper;
 import com.sirnoob.productservice.repository.IMainCategoryRepository;
 import com.sirnoob.productservice.repository.IProductRepository;
 import com.sirnoob.productservice.repository.ISubCategoryRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,37 +29,55 @@ public class ProductServiceImpl implements IProductService {
   private final IMainCategoryRepository iMainCategoryRepository;
   private final ISubCategoryRepository iSubCategoryRepository;
   private final IProductRepository iProductRepository;
-
+  private final IProductMapper iProductMapper;
 
   @Override
   public ProductResponse createProduct(ProductRequest productRequest) {
+
+    MainCategory mainCategory = getMainCategoryById(productRequest.getMainCategoryId());
+
+    Set<SubCategory> subCategories = getSubcategoriesById(productRequest.getSubCategoriesId());
     
-    MainCategory mainCategory = iMainCategoryRepository.findById(productRequest.getMainCategoryId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    Product product = iProductMapper.mapProductRequestToProduct(productRequest, mainCategory, subCategories);
 
-    return null;
+    product.setProductStatus("CREATED");
+    product.setCreateAt(LocalDate.now());
+
+    return iProductMapper.mapProductToProductResponse(iProductRepository.save(product));
   }
 
   @Override
-  public ProductInvoiceResponse getProductById(Long productId) {
-    // TODO Auto-generated method stub
-    return null;
+  public ProductInvoiceResponse getProductForInvoice(Long productBarCode) {
+    return iProductMapper.mapProductToProductInvoiceResponse(getProductByBarCode(productBarCode));
   }
 
   @Override
-  public ProductResponse getProductByBarCode(Long productBarCode) {
-    // TODO Auto-generated method stub
-    return null;
+  public Product getProductByBarCode(Long productBarCode) {
+    return iProductRepository.findByProductBarCode(productBarCode).orElseThrow(
+        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found with Bar Code " + productBarCode));
   }
 
   @Override
   public ProductResponse updateProduct(ProductRequest productRequest) {
-    // TODO Auto-generated method stub
-    return null;
+
+
+    Product product = iProductRepository.findById(productRequest.getProductId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found with Id " + productRequest.getProductId()));
+
+    product.setProductBarCode(productRequest.getProductBarCode());
+    product.setProductName(productRequest.getProductName());
+    product.setProductDescription(productRequest.getProductDescription());
+    product.setProductStock(productRequest.getProductStock());
+    product.setProductPrice(productRequest.getProductPrice());
+    product.setProductStatus("UPDATED");
+    product.setMainCategory(getMainCategoryById(productRequest.getMainCategoryId()));
+    product.setSubCategories(getSubcategoriesById(productRequest.getSubCategoriesId()));
+
+    return iProductMapper.mapProductToProductResponse(iProductRepository.save(product));
   }
 
   @Override
   public ProductResponse updateProductStock(Long productBarCode, Integer quantity) {
-    // TODO Auto-generated method stub
+
     return null;
   }
 
@@ -71,6 +97,26 @@ public class ProductServiceImpl implements IProductService {
   public List<ProductResponse> getProductsBySubCategoryId(Long subCategoryId) {
     // TODO Auto-generated method stub
     return null;
+  }
+
+
+
+  private MainCategory getMainCategoryById(Long mainCategoryId) {
+
+    return iMainCategoryRepository.findById(mainCategoryId).orElseThrow(
+        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Main Category Not Found with Id " + mainCategoryId));
+  }
+
+  private Set<SubCategory> getSubcategoriesById(Long[] subCategoriesId) {
+
+    Set<SubCategory> subCategories = new HashSet<>();
+
+    for (Long sc : subCategoriesId) {
+      subCategories.add(iSubCategoryRepository.findById(sc).orElseThrow(
+          () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sub Category Not Found with Id " + sc)));
+    }
+
+    return subCategories;
   }
 
 }
