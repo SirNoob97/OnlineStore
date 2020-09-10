682fa1b8d471c5d2056d8451e433a79fb2d56a7e
Martin <martinald467@gmail.com>
2020-09-10 21:26:04 +0200

    import innecesario

--- product-service/src/main/java/com/sirnoob/productservice/entity/Product.java
+++ product-service/src/main/java/com/sirnoob/productservice/entity/Product.java
@@ -3,7 +3,6 @@ package  com.sirnoob.productservice.entity;
 import java.time.LocalDate;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
