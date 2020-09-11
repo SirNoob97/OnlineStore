ab94990f6b4d27b9dae596f86dd46cc9d936bec1
Martin <martinald467@gmail.com>
2020-09-11 21:35:55 +0200

    agregadas notaciones de contructores para evitar excepciones en el momento de buscar productos por categoria principal ya que uso jpql en la consulta para ahorrame la clase mapper

--- product-service/src/main/java/com/sirnoob/productservice/entity/MainCategory.java
+++ product-service/src/main/java/com/sirnoob/productservice/entity/MainCategory.java
@@ -9,9 +9,13 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 @Entity
