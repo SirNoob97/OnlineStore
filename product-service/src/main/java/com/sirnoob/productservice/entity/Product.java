502db5f7d2cf6a5301069fd020ba764e7e97455e
Martin <martinald467@gmail.com>
2020-09-11 21:37:36 +0200

    agregadas notaciones para evitar excepciones como con las categorias pero en este caso es para el mapeo en el repositorio para dar respuestas de lectura de productos

--- product-service/src/main/java/com/sirnoob/productservice/entity/Product.java
+++ product-service/src/main/java/com/sirnoob/productservice/entity/Product.java
@@ -16,9 +16,13 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 @Entity
