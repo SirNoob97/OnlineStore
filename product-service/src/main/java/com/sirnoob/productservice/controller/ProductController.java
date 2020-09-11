011a13ecb7adec84853c04b9251173d24e0d6255
Martin <martinald467@gmail.com>
2020-09-11 21:52:31 +0200

    creados los metodos de lectura nuevos, en el controllador

--- product-service/src/main/java/com/sirnoob/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/sirnoob/productservice/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.sirnoob.productservice.controller;
 
+import java.util.List;
+
+import com.sirnoob.productservice.dto.ProductListView;
 import com.sirnoob.productservice.service.IProductService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,25 @@ public class ProductController {
   public ResponseEntity<Void> updateProductStock(@PathVariable Long productBarCode, @RequestParam(required = true) Integer quantity){
     iProductService.updateProductStock(productBarCode, quantity);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/getallproducts/{page}")
+  public ResponseEntity<List<ProductListView>> getAllProducts(@PathVariable int page){
+    return ResponseEntity.ok().body(iProductService.getAllProducts(page));
+  }
+
+  @GetMapping("/findbymaincategory/{page}")
+  public ResponseEntity<List<ProductListView>> findProductsByMainCategoryId(@RequestParam(required = true) String mainCategoryName, @PathVariable int page){
+    return ResponseEntity.ok().body(iProductService.getProductsByMainCategory(mainCategoryName, page));
+  }
+
+  @GetMapping("/findbyproductname/{productName}")
+  public ResponseEntity<ProductListView> findProductByProductName(@PathVariable(required = true) String productName){
+    return ResponseEntity.ok().body(iProductService.getProductByName(productName));
+  }
+
+  @GetMapping("/listbyproductname/{page}")
+  public ResponseEntity<List<ProductListView>> listByProductName(@RequestParam(required = true) String productName, @PathVariable int page){
+    return ResponseEntity.ok().body(iProductService.listByName(productName, page));
   }
 }
