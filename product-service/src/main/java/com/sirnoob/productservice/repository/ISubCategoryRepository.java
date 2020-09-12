92b78a2699d7aead1beb7e242a0610afd18b6a88
Martin <martinald467@gmail.com>
2020-09-12 21:43:13 +0200

    dejare este metodo asi por ahora ya que quiero probar algunas cosas despues

--- product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
@@ -3,6 +3,7 @@ package com.sirnoob.productservice.repository;
 import com.sirnoob.productservice.entity.SubCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.Optional;
 @Repository
 public interface ISubCategoryRepository extends JpaRepository<SubCategory, Long>{
 
-  public Optional<SubCategory> findBySubCategoryName(String subCategoryName);
+  //@Query("SELECT sc.subCategoryId, sc.subCategoryName FROM SubCategory sc WHERE sc.subCategoryName = :subCategoryName")
+  public Optional<SubCategory> findBySubCategoryName(@Param("subCategoryName") String subCategoryName);
 
   public List<SubCategory> findByMainCategoryMainCategoryId(Long mainCategoryId);
 }
