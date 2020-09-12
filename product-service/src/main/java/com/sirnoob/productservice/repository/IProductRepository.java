81f6476bf7585df1299cc690eaf3d28aa2e9cad0
Martin <martinald467@gmail.com>
2020-09-12 21:44:40 +0200

    cambio en el retorno de algunos metodos para usar de apollo a otros metodos en el servicio --pendiente

--- product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import com.sirnoob.productservice.dto.ProductListView;
 import com.sirnoob.productservice.entity.MainCategory;
 import com.sirnoob.productservice.entity.Product;
+import com.sirnoob.productservice.entity.SubCategory;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -19,7 +21,7 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
   @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p")
   public List<ProductListView> getAll(Pageable pageable);
 
-  public Optional<ProductListView> findByProductName(String productName);
+  public Optional<Product> findByProductName(String productName);
 
   @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p WHERE p.productName LIKE :productName%")
   public List<ProductListView> listByName(@Param("productName") String productName, Pageable pageable);
@@ -28,7 +30,10 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
 
   public List<ProductListView> findByMainCategory(MainCategory mainCategory, Pageable pageable);
 
+  @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p WHERE :subCategory MEMBER p.subCategories")
+  public ProductListView findBySubCategory(@Param("subCategory") SubCategory subCategory);
+
   @Modifying
   @Query(value = "UPDATE products SET product_stock = :stock WHERE product_bar_code = :productBarCode", nativeQuery = true)
-  public void updateProductStockForProductBarCode(@Param("stock") Integer stock, @Param("productBarCode") Long productBarCode);
+  public void updateProductStockByProductBarCode(@Param("stock") Integer stock, @Param("productBarCode") Long productBarCode);
 }
