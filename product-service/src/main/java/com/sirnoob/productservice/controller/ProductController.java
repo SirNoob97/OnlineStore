d524595182c60346ca49d739e9fd1bebf3a7e9f9
Martin <martinald467@gmail.com>
2020-09-13 19:47:24 +0200

    mejorados todos end points ahora respetan un poco mas las buenas practicas --pendiente esto ultimo
    esto ultimo tambien ocasiono que algunos pathvariables se convirtieran el requestparam, creados metodos para obtener info para factura, responses para los empleados, eliminacion por id y edicion de productos

--- product-service/src/main/java/com/sirnoob/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/sirnoob/productservice/controller/ProductController.java
@@ -12,7 +12,9 @@ import com.sirnoob.productservice.dto.ProductResponse;
 import com.sirnoob.productservice.dto.ProductView;
 import com.sirnoob.productservice.service.IProductService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,46 +33,61 @@ public class ProductController {
 
   private final IProductService iProductService;
 
-  @PostMapping("/createproduct")
+  @PostMapping
   public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest){
     return ResponseEntity.ok().body(iProductService.createProduct(productRequest));
   }
 
-  @PutMapping("/updateproductstock/{productBarCode}/")
-  public ResponseEntity<Void> updateProductStock(@PathVariable Long productBarCode, @RequestParam(required = true) Integer quantity){
-    iProductService.updateProductStock(productBarCode, quantity);
-    return ResponseEntity.noContent().build();
+  @PutMapping("/{productId}")
+  public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductRequest productRequest){
+    return ResponseEntity.ok().body(iProductService.updateProduct(productId, productRequest));
   }
 
-  @GetMapping("/getproductforinvoice")
-  public ResponseEntity<ProductInvoiceResponse> getProductForInvoice(@RequestParam(required = false, defaultValue = "0") Long productBarCode, @RequestParam(required = false, defaultValue = " ") String productName){
+  @PutMapping("/{productBarCode}/stock")
+  public ResponseEntity<String> updateProductStock(@PathVariable Long productBarCode, @RequestParam(required = true) Integer quantity){
+   return iProductService.updateProductStock(productBarCode, quantity) > 0 ? ResponseEntity.noContent().build()
+                                                                            : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+  }
+
+  @GetMapping("/responses")
+  public ResponseEntity<ProductResponse> getProductResponse(@RequestParam(defaultValue = "0", required = false) Long productBarCode,
+                                                            @RequestParam(defaultValue = " ", required = false) String productName){
+    return ResponseEntity.ok().body(iProductService.getProductResponseByBarCodeOrProductName(productBarCode, productName));
+  }
+
+  @GetMapping("/invoices")
+  public ResponseEntity<ProductInvoiceResponse> getProductForInvoice(@RequestParam(required = true) Long productBarCode, @RequestParam(required = true) String productName){
     return ResponseEntity.ok().body(iProductService.getProductForInvoiceResponse(productBarCode, productName));
   }
 
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<Void> deleteProductById(@PathVariable Long productId){
+    iProductService.deleteProductById(productId);
+    return ResponseEntity.noContent().build();
+  }
 
-
-  @GetMapping("/getallproducts/{page}")
-  public ResponseEntity<List<ProductListView>> getAllProducts(@PathVariable int page){
+  @GetMapping
+  public ResponseEntity<List<ProductListView>> listAllProducts(@RequestParam(defaultValue = "0") int page){
     return ResponseEntity.ok().body(iProductService.getPageOfProductListView(page));
   }
 
-  @GetMapping("/findbymaincategory/{page}")
-  public ResponseEntity<List<ProductListView>> findProductsByMainCategoryId(@RequestParam(required = true) String mainCategoryName, @PathVariable int page){
+  @GetMapping("/main-categories/{page}")
+  public ResponseEntity<List<ProductListView>> listProductsByMainCategoryId(@RequestParam(required = true) String mainCategoryName, @PathVariable int page){
     return ResponseEntity.ok().body(iProductService.getProductListViewByMainCategory(mainCategoryName, page));
   }
 
-  @GetMapping("/findbysubcategories")
-  public ResponseEntity<Set<ProductListView>> findBySubCategories(@RequestBody String[] subCategories){
+  @GetMapping("/sub-categories")
+  public ResponseEntity<Set<ProductListView>> listProductsBySubCategories(@RequestBody String[] subCategories){
     return ResponseEntity.ok().body(iProductService.getProductListViewBySubCategory(subCategories));
   }
 
-  @GetMapping("/findbyproductname/{productName}")
-  public ResponseEntity<ProductView> findProductByProductName(@PathVariable(required = true) String productName){
+  @GetMapping("/names")
+  public ResponseEntity<ProductView> getProductByProductName(@RequestParam(required = true) String productName){
     return ResponseEntity.ok().body(iProductService.findProductViewByName(productName));
   }
 
-  @GetMapping("/listbyproductname/{page}")
-  public ResponseEntity<List<ProductListView>> listByProductName(@RequestParam(required = true) String productName, @PathVariable int page){
+  @GetMapping("/names/{page}")
+  public ResponseEntity<List<ProductListView>> listByProductNameCoincidences(@RequestParam(required = true) String productName, @PathVariable int page){
     return ResponseEntity.ok().body(iProductService.getProductListViewByName(productName, page));
   }
 }
