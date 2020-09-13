88ed581e73649784bfbb7dfd2aac4c2d2dd42ec6
Martin <martinald467@gmail.com>
2020-09-13 21:15:53 +0200

    agregado metodo para actualizar el nombre de las categorias

--- product-service/src/main/java/com/sirnoob/productservice/repository/IMainCategoryRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/IMainCategoryRepository.java
@@ -5,10 +5,17 @@ import java.util.Optional;
 import com.sirnoob.productservice.entity.MainCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMainCategoryRepository extends JpaRepository<MainCategory, Long>{
-  
+
   public Optional<MainCategory> findByMainCategoryName(String categoryName);
+
+  @Modifying
+  @Query(value = "UPDATE main_categories SET category_name = :categoryName WHERE category_id = :categoryId", nativeQuery = true)
+  public int updateMainCategoryName(@Param("categoryName") String categoryName, @Param("categoryId") Long categoryId);
 }
