23b7dbd08af71105834f1286893ce654a8e69515
Martin <martinald467@gmail.com>
2020-09-14 19:59:24 +0200

    eliminados al final no los necesite

--- product-service/src/main/java/com/sirnoob/productservice/mapper/ISubCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sirnoob.productservice.mapper;
-
-import com.sirnoob.productservice.dto.SubCategoryResponse;
-import com.sirnoob.productservice.entity.SubCategory;
-
-public interface ISubCategoryMapper {
-
-  public SubCategoryResponse mapSuCategoryToSubCategoryResponse(SubCategory subCategory);
-}
