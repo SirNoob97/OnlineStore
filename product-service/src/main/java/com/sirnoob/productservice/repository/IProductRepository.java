9ff1ce8eea2732b4b64b73928375f5c1227edb53
Martin <martinald467@gmail.com>
2020-09-15 20:05:13 +0200

    corregido retorno de busqueda por sub categoria esto siempre debe ser una lista no tiene sentido que sea una entidad unitaria
    y agrefado metodo para buscar por categoria principal, no por id de categoria principal, directamente por categoria, esto debido a una excepcion a la hora de eliminar categorias principales, ya que al hacer esto tambien deben eliminarse los productos y las subcategorias pertenecientes a esa categoria, me parece logico, pero cuando un producto que estaba referenciado con una sub categoria que no fue eliminada saltaba una excepcion de integridad de datos aun cuando los productos no necesitan desligarse de las sub categorias cuando se eliminan caso contrario para las sub categorias que si necesito hacerlo

--- product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
@@ -15,12 +15,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Long>{
 
   public Optional<Product> findByProductName(String productName);
 
   public Optional<Product> findByProductBarCodeOrProductName(Long productBarCode, String productName);
+
+  public List<Product> findByMainCategory(MainCategory mainCategory);
 
   @Query("SELECT new com.sirnoob.productservice.dto.ProductInvoiceResponse(p.productName, p.productPrice) FROM Product p WHERE p.productName = :productName AND p.productBarCode = :productBarCode")
   public Optional<ProductInvoiceResponse> findProductForInvoice(@Param("productBarCode") Long productBarCode, @Param("productName") String productName);
@@ -34,7 +38,7 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
   public Page<ProductListView> findByMainCategory(MainCategory mainCategory, Pageable pageable);
 
   @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p WHERE :subCategory MEMBER p.subCategories")
-  public ProductListView findBySubCategory(@Param("subCategory") SubCategory subCategory);
+  public List<ProductListView> findBySubCategory(@Param("subCategory") SubCategory subCategory);
 
   @Modifying
   @Query(value = "UPDATE products SET product_stock = :stock WHERE product_bar_code = :productBarCode", nativeQuery = true)
