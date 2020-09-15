9925fd8fc77802ed848faf86096018c2c7acc5b4
Martin <martinald467@gmail.com>
2020-09-15 20:08:12 +0200

    modificado metodo de eliminacion de categorias principales ahora limpiara las coleccion de las relaciones entre productos y sub categorias con el fin de evitar una excepcion de integridad de datos por parte de los productos, agregado reconocimiento de tipo lazy para el servicio de los productos ya que sin esto formaria un ciclo de scaneo

--- product-service/src/main/java/com/sirnoob/productservice/service/MainCategoryServiceImpl.java
+++ product-service/src/main/java/com/sirnoob/productservice/service/MainCategoryServiceImpl.java
@@ -8,19 +8,26 @@ import javax.transaction.Transactional;
 import com.sirnoob.productservice.entity.MainCategory;
 import com.sirnoob.productservice.repository.IMainCategoryRepository;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Service
 public class MainCategoryServiceImpl implements IMainCategoryService {
 
+  private final IProductService iProductService;
   private final ISubCategoryService iSubCategoryService;
   private final IMainCategoryRepository iMainCategoryRepository;
+
+
+  public MainCategoryServiceImpl(@Lazy IProductService iProductService, ISubCategoryService iSubCategoryService,
+      IMainCategoryRepository iMainCategoryRepository) {
+    this.iProductService = iProductService;
+    this.iSubCategoryService = iSubCategoryService;
+    this.iMainCategoryRepository = iMainCategoryRepository;
+  }
 
   @Override
   public String createMainCategory(MainCategory mainCategory) {
@@ -37,8 +44,8 @@ public class MainCategoryServiceImpl implements IMainCategoryService {
   @Override
   public void deleteMainCategory(Long mainCategoryId) {
     MainCategory mainCategory = getMainCategoryById(mainCategoryId);
-    iSubCategoryService.getSubCategoryByMainCategory(mainCategory).forEach(sc -> iSubCategoryService.deleteSubCategory(sc.getSubCategoryId()));
-
+    iSubCategoryService.getSubCategoryByMainCategory(mainCategory).forEach(sc -> sc.getProducts().clear());
+    iProductService.getProductByMainCategory(mainCategory).forEach(prs -> prs.getSubCategories().clear());
     iMainCategoryRepository.delete(mainCategory);
   }
 
