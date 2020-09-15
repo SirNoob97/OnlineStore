24c31663e0c855240e44e39ebed6115172c5558d
Martin <martinald467@gmail.com>
2020-09-15 19:59:27 +0200

    corregido al cambiar de rama y renombrar commits antiguos puede que haya roto esta interface ya esta como deberia ->
    eliminadas notaciones innecesarias de la busqueda por nombre, agregado metodo para buscar por categoria principal, y mejorado metodo de actualizacion de nombre

--- product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
@@ -1,19 +1,25 @@
 package com.sirnoob.productservice.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.sirnoob.productservice.entity.MainCategory;
 import com.sirnoob.productservice.entity.SubCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ISubCategoryRepository extends JpaRepository<SubCategory, Long>{
 
-  //@Query("SELECT sc.subCategoryId, sc.subCategoryName FROM SubCategory sc WHERE sc.subCategoryName = :subCategoryName")
-  public Optional<SubCategory> findBySubCategoryName(@Param("subCategoryName") String subCategoryName);
+  public Optional<SubCategory> findBySubCategoryName(String subCategoryName);
 
-  public List<SubCategory> findByMainCategoryMainCategoryId(Long mainCategoryId);
+  public List<SubCategory> findByMainCategory(MainCategory subCategory);
+
+  @Modifying
+  @Query(value = "UPDATE sub_categories SET sub_category_name = :subCategoryName WHERE sub_category_id = :subCategoryId", nativeQuery = true)
+  public int updateSubCategoryName(@Param("subCategoryName") String subCategoryName, @Param("subCategoryId") Long subCategoryId);
 }
