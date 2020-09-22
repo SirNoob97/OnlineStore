4e790294b35b947a831141a1720890de01baa484
Martin <martinald467@gmail.com>
2020-09-22 20:19:10 +0200

    cambio en el metodo de acceso del setter del id de las categorias principales para que no se pueda entitar con el metodo save del repositorio, agrandado el maximo de caracteres para el nombre a 50

--- product-service/src/main/java/com/sirnoob/productservice/entity/MainCategory.java
+++ product-service/src/main/java/com/sirnoob/productservice/entity/MainCategory.java
@@ -7,11 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,10 +26,12 @@ public class MainCategory {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "category_id")
+  @Column(name = "category_id", updatable = false)
+  @Setter(AccessLevel.NONE)
   private Long mainCategoryId;
 
   @NotEmpty(message = "The Category Name is required")
+  @Size(max = 50, message = "The Name must be a maximum of {max} characters")
   @Column(name = "category_name", nullable = false, unique = true, length = 50)
   private String mainCategoryName;
 }
