66f2d441b8b56f1aaef33faafd2295021bd1c537
Martin <martinald467@gmail.com>
2020-09-26 19:02:31 +0200

    mejorado los medotos para obtener productos mediante categoria principal ahora solo se necesitara el id de la categoria

--- product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/IProductRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import com.sirnoob.productservice.dto.ProductInvoiceResponse;
 import com.sirnoob.productservice.dto.ProductListView;
-import com.sirnoob.productservice.entity.MainCategory;
 import com.sirnoob.productservice.entity.Product;
 import com.sirnoob.productservice.entity.SubCategory;
 
@@ -24,7 +23,7 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
 
   public Optional<Product> findByProductBarCodeOrProductName(Long productBarCode, String productName);
 
-  public List<Product> findByMainCategory(MainCategory mainCategory);
+  public List<Product> findByMainCategoryMainCategoryId(Long mainCategoryId);
 
   @Query("SELECT new com.sirnoob.productservice.dto.ProductInvoiceResponse(p.productName, p.productPrice) FROM Product p WHERE p.productName = :productName AND p.productBarCode = :productBarCode")
   public Optional<ProductInvoiceResponse> findProductForInvoice(@Param("productBarCode") Long productBarCode, @Param("productName") String productName);
@@ -32,10 +31,9 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
   @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p")
   public Page<ProductListView> getAll(Pageable pageable);
 
-  @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :productName, '%'))")
-  public Page<ProductListView> listByNameMatches(@Param("productName") String productName, Pageable pageable);
+  public Page<ProductListView> findByProductNameContainingIgnoreCase(String productName, Pageable pageable);
 
-  public Page<ProductListView> findByMainCategory(MainCategory mainCategory, Pageable pageable);
+  public Page<ProductListView> findByMainCategoryMainCategoryId(Long mainCategory, Pageable pageable);
 
   @Query("SELECT new com.sirnoob.productservice.dto.ProductListView(p.productName, p.productDescription, p.productPrice) FROM Product p WHERE :subCategory MEMBER p.subCategories")
   public List<ProductListView> findBySubCategory(@Param("subCategory") SubCategory subCategory);
@@ -43,4 +41,5 @@ public interface IProductRepository extends JpaRepository<Product, Long>{
   @Modifying(clearAutomatically = true)
   @Query("UPDATE Product AS p SET p.productStock = :stock WHERE p.productBarCode = :productBarCode")
   public int updateProductStockByProductBarCode(@Param("stock") Integer stock, @Param("productBarCode") Long productBarCode);
+
 }
