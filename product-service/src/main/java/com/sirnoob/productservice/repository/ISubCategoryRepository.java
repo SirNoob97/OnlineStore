66f47ff61126002af9a95d6e773e6f64479e6ce8
Martin <martinald467@gmail.com>
2020-09-28 20:42:50 +0200

    cambio en el metodo de obtencion de sub categorias mediante su categoria principal ahora solo se necesita el id de dicha categoria

--- product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
+++ product-service/src/main/java/com/sirnoob/productservice/repository/ISubCategoryRepository.java
@@ -3,21 +3,20 @@ package com.sirnoob.productservice.repository;
 import java.util.List;
 import java.util.Optional;
 
-import com.sirnoob.productservice.entity.MainCategory;
-import com.sirnoob.productservice.entity.SubCategory;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.sirnoob.productservice.entity.SubCategory;
+
 @Repository
 public interface ISubCategoryRepository extends JpaRepository<SubCategory, Long>{
 
   public Optional<SubCategory> findBySubCategoryName(String subCategoryName);
 
-  public List<SubCategory> findByMainCategory(MainCategory subCategory);
+  public List<SubCategory> findByMainCategoryMainCategoryId(Long mainCategoryId);
 
   @Modifying(clearAutomatically = true)
   @Query("UPDATE SubCategory AS s SET s.subCategoryName = :subCategoryName WHERE s.subCategoryId = :subCategoryId")
