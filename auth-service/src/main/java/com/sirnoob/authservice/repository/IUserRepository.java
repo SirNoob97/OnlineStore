d01cf7c165b9d5df26339f9a047ed08abbdc018f
Martin <martinald467@gmail.com>
2020-10-09 20:50:32 +0200

    agregado metodo de busqueda por nombre

--- auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
+++ auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
@@ -4,6 +4,9 @@ import com.sirnoob.authservice.entity.User;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import reactor.core.publisher.Mono;
+
 public interface IUserRepository extends ReactiveCrudRepository<User, Long>{
 
+  public Mono<User> findByUserName(String userName);
 }
