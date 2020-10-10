0174fa276b974f3c43f7f419f65889a585c006e7
Martin <martinald467@gmail.com>
2020-10-10 21:29:56 +0200

    renombre del paquete de los modelos me parece mas apropiado ya que no se da informacion de como sera o es la tabla en la base de datos

---
+++ auth-service/src/main/java/com/sirnoob/authservice/domain/Role.java
@@ -0,0 +1,6 @@
+package com.sirnoob.authservice.entity;
+
+public enum Role {
+
+  CUSTOMER, EMPLOYEE, ADMIN;
+}
---
+++ auth-service/src/main/java/com/sirnoob/authservice/domain/User.java
@@ -0,0 +1,63 @@
+package com.sirnoob.authservice.domain;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(value = "users")
+public class User implements UserDetails{
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  private Long id;
+  private String userName;
+  private String password;
+  private String email;
+  private Role role;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return this.userName;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+
+
+}
