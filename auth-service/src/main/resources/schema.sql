02e8e5979043df1996f0f5fcabea5635cd4b4837
Martin <martinald467@gmail.com>
2020-10-10 21:27:59 +0200

    renombradas las columnas en la tabla de usuarios debido al renombre del objeto

--- auth-service/src/main/resources/schema.sql
+++ auth-service/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
       id identity NOT NULL,
       user_name character varying(60) NOT NULL,
-      user_password character varying(60) NOT NULL,
-      user_role character varying(10) NOT NULL,
-      check (user_role in ('CUSTOMER', 'EMPLOYEE', 'ADMIN')));
+      password character varying(60) NOT NULL,
+      role character varying(10) NOT NULL,
+      check (role in ('CUSTOMER', 'EMPLOYEE', 'ADMIN')));
