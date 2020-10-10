57572568a4843fb303c6e7a5621b2c5a0bad8805
Martin <martinald467@gmail.com>
2020-10-10 21:26:00 +0200

    agregada configuracion para el manejo de excepciones si el usuario no esta autorizado o no tiene acceso ya que no esta authenticado, authentication manager y security context repository

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.sirnoob.authservice.configuration;
 
+import com.sirnoob.authservice.security.AuthenticationManager;
+import com.sirnoob.authservice.security.SecurityContextRepository;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -8,14 +12,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
+  private final AuthenticationManager authenticationManager;
+  private final SecurityContextRepository securityContextRepository;
+
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-    return serverHttpSecurity.csrf().disable()
-                              .authorizeExchange().pathMatchers("/").permitAll().and().build();
+    //@formatter:off
+    return serverHttpSecurity.exceptionHandling()
+                              .authenticationEntryPoint(
+                                  (swe, aex) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))
+                              )
+                              .accessDeniedHandler(
+                                  (swe, aex) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN))
+                              )
+                              .and()
+                              .csrf().disable()
+                              .formLogin().disable()
+                              .httpBasic().disable()
+                              .authenticationManager(authenticationManager)
+                              .securityContextRepository(securityContextRepository)
+                              .authorizeExchange()
+                              .pathMatchers("/").permitAll()
+                              .and().build();
+    //@formatter:on
   }
 
   @Bean
