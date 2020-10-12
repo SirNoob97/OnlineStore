73c3c122cd2d6968b46cd440a5bbe6d38a064ca1
Martin <martinald467@gmail.com>
2020-10-12 20:40:37 +0200

    adaptado debido a la creacion de la interfaz de AuthServiceImpl

--- auth-service/src/main/java/com/sirnoob/authservice/handler/AuthHandler.java
+++ auth-service/src/main/java/com/sirnoob/authservice/handler/AuthHandler.java
@@ -4,7 +4,7 @@ import com.sirnoob.authservice.dto.AuthResponse;
 import com.sirnoob.authservice.dto.LoginRequest;
 import com.sirnoob.authservice.dto.SignUpRequest;
 import com.sirnoob.authservice.exception.CustomException;
-import com.sirnoob.authservice.service.AuthService;
+import com.sirnoob.authservice.service.IAuthService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,30 +13,26 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthHandler {
 
-  private final AuthService authService;
+  private final IAuthService iAuthService;
 
   private static final MediaType JSON = MediaType.APPLICATION_JSON;
 
-  public Mono<ServerResponse> signup(ServerRequest serverRequest){    
+  public Mono<ServerResponse> signup(ServerRequest serverRequest){
     Mono<SignUpRequest> body = serverRequest.bodyToMono(SignUpRequest.class);
-    Mono<AuthResponse> authResponse = body.flatMap(authService::signup);
+    Mono<AuthResponse> authResponse = body.flatMap(iAuthService::signup);
 
     return authResponse.flatMap(data -> ServerResponse.status(HttpStatus.CREATED).contentType(JSON).bodyValue(data));
-                        
-//                        .onErrorResume(error -> ServerResponse.badRequest().build());
   }
 
   public Mono<ServerResponse> login(ServerRequest serverRequest){
     Mono<LoginRequest> body = serverRequest.bodyToMono(LoginRequest.class);
-    Mono<AuthResponse> authResponse = body.flatMap(authService::login);
+    Mono<AuthResponse> authResponse = body.flatMap(iAuthService::login);
 
     return authResponse.flatMap(data -> ServerResponse.ok().contentType(JSON).bodyValue(data))
                         .switchIfEmpty(ServerResponse.notFound().build())
