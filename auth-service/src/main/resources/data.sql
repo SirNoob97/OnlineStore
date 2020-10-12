821fa0fa4627910e61123b2dc944062fd3275915
Martin <martinald467@gmail.com>
2020-10-12 20:52:49 +0200

    agregado un nuevo usuario para poder testeor los metodos de gestion de cuentas y agregado un empleado

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO users (user_name, password, email, role) VALUES ('user','user', 'useremail@mail.com', 'EMPLOYEE');
+INSERT INTO users (user_name, password, email, role) VALUES ('employee','$2a$10$ohLOHQxl6i9zhwHwgQI9U.qpRihIdQaBANWFqLV2cS67uHiwX5Kp.', 'useremail@mail.com', 'EMPLOYEE');
+INSERT INTO users (user_name, password, email, role) VALUES ('maldosd','$2a$10$KtgoKvlpJZj3H2stcz8hxOn9VsD0B0ETwwmVK6NbEL1ISrcpZw7q2', 'myawesome@email.com', 'ADMIN');
