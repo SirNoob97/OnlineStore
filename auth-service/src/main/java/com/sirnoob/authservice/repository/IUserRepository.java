e08fb32d5f0087af923a20f6db845f515e3b3d87
Martin <martinald467@gmail.com>
2020-10-12 20:41:50 +0200

    agregados metodos de actualizacion de password y eliminacion por id --pendiente testeo

--- auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
+++ auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
@@ -2,6 +2,8 @@ package com.sirnoob.authservice.repository;
 
 import com.sirnoob.authservice.domain.User;
 
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import reactor.core.publisher.Mono;
@@ -9,4 +11,12 @@ import reactor.core.publisher.Mono;
 public interface IUserRepository extends ReactiveCrudRepository<User, Long>{
 
   public Mono<User> findByUserName(String userName);
+
+  @Modifying
+  @Query("UPDATE users SET users.password = :password WHERE users.id = :userId")
+  public Mono<Integer> updatePasswordById(Long userId, String password);
+
+  @Modifying
+  @Query("DELETE FROM users WHERE users.id = :userId")
+  public Mono<Integer> deleteAccountById(Long userId);
 }
