8d25f412d608d4c7dff79610bb407eed05f99658
Martin <martinald467@gmail.com>
2020-10-12 20:44:14 +0200

    modificado el metodo de generacion de tokens ya que olvide agregar los roles al token y agregado algoritmo para el token generado por username --este ultimo pendiente de test

--- auth-service/src/main/java/com/sirnoob/authservice/security/JwtProvider.java
+++ auth-service/src/main/java/com/sirnoob/authservice/security/JwtProvider.java
@@ -11,6 +11,9 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
@@ -22,7 +25,10 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class JwtProvider {
 
@@ -45,13 +51,18 @@ public class JwtProvider {
 
 
   public String generateToken(User user) {
-    return Jwts.builder().setSubject(user.getUsername()).signWith(getPrivateKey())
-        .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis))).compact();
+    Map<String, Object> claims = new HashMap<>();
+    claims.put("role", List.of(user.getRole()));
+log.info("role name {}", user.getRole().name());
+log.info("role {}", user.getRole());
+    return Jwts.builder().setClaims(claims).setSubject(user.getUsername()).setIssuedAt(new Date())
+        .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis))).signWith(getPrivateKey()).compact();
   }
 
   public String generateTokenWithUserName(String userName) {
-    return Jwts.builder().setSubject(userName).setIssuedAt(Date.from(Instant.now())).signWith(getPrivateKey())
-        .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis))).compact();
+    return Jwts.builder().setSubject(userName).setIssuedAt(Date.from(Instant.now()))
+        .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
+        .signWith(getPrivateKey(), SignatureAlgorithm.HS512).compact();
   }
 
   public String getUsernameFromJwt(String token) {
