fd872767dda30868944a473d064d2b4e00a8b701
Martin <martinald467@gmail.com>
2020-10-13 19:33:53 +0200

    agregado campo de id ya que se necesitara para poder usar los endpoints de modificacion

--- auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
@@ -4,25 +4,31 @@ import com.sirnoob.authservice.domain.Role;
 
 public class AccountView {
 
+  private Long id;
   private String name;
   private String email;
   private Role role;
 
-  public AccountView(String name, String email, Role role) {
+  public AccountView(Long id, String name, String email, Role role) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.role = role;
   }
 
+  public Long getId() {
+    return this.id;
+  }
+
   public String getName() {
-    return name;
+    return this.name;
   }
 
   public String getEmail() {
-    return email;
+    return this.email;
   }
 
   public Role getRole() {
-    return role;
+    return this.role;
   }
 }
