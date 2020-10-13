15ab7c3c82fd7164c16b7661802da8489736c9fd
Martin <martinald467@gmail.com>
2020-10-13 19:49:25 +0200

    agregada configuracion de h2 con esto funciona la connecion prefiero tener esto a que se conecte sin saber como y talves use el connection factory para otras features en el futuro

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
@@ -4,17 +4,24 @@ package com.sirnoob.authservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
 import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 
 @Configuration
 @EnableR2dbcRepositories
-public class H2Config {
+public class H2Config extends AbstractR2dbcConfiguration{
 
+  @Bean
+  @Override
+  public ConnectionFactory connectionFactory(){
+     return ConnectionFactories.get("r2dbc:h2:mem:///test?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+  }
 
   @Bean
   public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){
