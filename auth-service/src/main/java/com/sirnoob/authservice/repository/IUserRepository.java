88fffb050efb3145eeaaf248d95f841942363494
Martin <martinald467@gmail.com>
2020-10-14 17:31:57 +0200

    cambio del tipo de repositorio a heredar con este nuevo puedo ordenar con la llamada del findAll no se si usarlo pero lo dejare de momento

--- auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
+++ auth-service/src/main/java/com/sirnoob/authservice/repository/IUserRepository.java
@@ -4,11 +4,13 @@ import com.sirnoob.authservice.domain.User;
 
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 
 import reactor.core.publisher.Mono;
 
-public interface IUserRepository extends ReactiveCrudRepository<User, Long>{
+@Repository
+public interface IUserRepository extends R2dbcRepository<User, Long> {
 
   public Mono<User> findByUserName(String userName);
 
