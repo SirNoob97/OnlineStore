c1bca6983a2ae49347760b38aa8e640206a1dff5
Martin <martinald467@gmail.com>
2020-10-15 19:19:51 +0200

    agregados constraint unique al username y al email

--- auth-service/src/main/resources/schema.sql
+++ auth-service/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
       id identity NOT NULL,
-      user_name character varying(60) NOT NULL,
+      user_name character varying(60) NOT NULL UNIQUE,
       password character varying(60) NOT NULL,
-      email character varying(60) NOT NULL,
+      email character varying(60) NOT NULL UNIQUE,
       role character varying(10) NOT NULL,
       check (role in ('CUSTOMER', 'EMPLOYEE', 'ADMIN')));
