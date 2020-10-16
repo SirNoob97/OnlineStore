d66af5ee0f32d0d5298f452cc991da7d3cda2ad2
Martin <martinald467@gmail.com>
2020-10-16 20:10:33 +0200

    cambio del schema para entenderlo mejor

--- auth-service/src/main/resources/schema.sql
+++ auth-service/src/main/resources/schema.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
-      id identity NOT NULL,
-      user_name character varying(60) NOT NULL UNIQUE,
+      id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
+      user_name character varying(60) NOT NULL,
       password character varying(60) NOT NULL,
-      email character varying(60) NOT NULL UNIQUE,
+      email character varying(60) NOT NULL,
       role character varying(10) NOT NULL,
+      PRIMARY KEY(id),
+      CONSTRAINT unique_username UNIQUE (user_name),
+      CONSTRAINT unique_email UNIQUE (email),
       check (role in ('CUSTOMER', 'EMPLOYEE', 'ADMIN')));
