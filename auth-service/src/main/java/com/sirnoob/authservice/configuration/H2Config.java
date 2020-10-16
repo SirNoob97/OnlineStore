dbdbbf0ad5472f815fe888ac6cb64559db495cd1
Martin <martinald467@gmail.com>
2020-10-16 20:09:15 +0200

    agregadas opciones para la url de coneccion con h2 se supone que deberia enteder querys de postgresql pero sigue sin entender el ADD

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
 import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
@@ -21,7 +23,7 @@ public class H2Config extends AbstractR2dbcConfiguration{
   @Bean
   @Override
   public ConnectionFactory connectionFactory(){
-     return ConnectionFactories.get("r2dbc:h2:mem:///test?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+     return ConnectionFactories.get("r2dbc:h2:mem:///accounts?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE");
   }
 
   @Bean
