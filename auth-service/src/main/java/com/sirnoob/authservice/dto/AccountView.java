c0519a777018b67befad954994d6677996d9e405
Martin <martinald467@gmail.com>
2020-10-17 19:10:49 +0200

    agregado equals and hash code por testeos del servicio

--- auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
@@ -31,4 +31,46 @@ public class AccountView {
   public Role getRole() {
     return this.role;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((role == null) ? 0 : role.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AccountView other = (AccountView) obj;
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    } else if (!email.equals(other.email))
+      return false;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (role != other.role)
+      return false;
+    return true;
+  }
+
+
 }
