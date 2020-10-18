6d7dd3a66d131bfdd68ceaa0001666b832a06f63
Martin <martinald467@gmail.com>
2020-10-18 20:27:28 +0200

    agregado ruteo para los metodos nuevos del handler de authenticacion

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/Router.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/Router.java
@@ -25,7 +25,9 @@ public class Router {
   @Bean
   public RouterFunction<ServerResponse> authEndPoints (AuthHandler authHandler){
     return RouterFunctions.route(POST("/auth/signup").and(accept(JSON).and(contentType(JSON))), authHandler::signup)
-                          .andRoute(POST("/auth/login").and(accept(JSON).and(contentType(JSON))), authHandler::login);
+                          .andRoute(POST("/auth/login").and(accept(JSON).and(contentType(JSON))), authHandler::login)
+                          .andRoute(POST("/auth/logout").and(accept(JSON)), authHandler::logout)
+                          .andRoute(POST("/auth/refresh-token").and(accept(JSON)).and(contentType(JSON)), authHandler::refreshToken);
   }
 
   @Bean
