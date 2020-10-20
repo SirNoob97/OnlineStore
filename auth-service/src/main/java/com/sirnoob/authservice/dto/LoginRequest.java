7fd4809962604c88d4ebcc8772cd29f415bd2e4f
Martin <martinald467@gmail.com>
2020-10-20 19:49:52 +0200

    eliminadas notaciones de javax validation no voy a perder mas tiempo con eso mejor hare una clase para eso

--- auth-service/src/main/java/com/sirnoob/authservice/dto/LoginRequest.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/LoginRequest.java
@@ -1,16 +1,8 @@
 package com.sirnoob.authservice.dto;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
 public class LoginRequest {
 
-  @NotEmpty(message = "The UserNames is Required!!")
-  @Size(max = 60, message = "The Maximum Characters Allowed For The Username Is {max}!!")
   private String userName;
-
-  @NotEmpty(message = "The Password is Required!!")
-  @Size(max = 60, message = "The Maximum Characters Allowed For The Password Is {max}!!")
   private String password;
 
   public LoginRequest(String userName, String password) {
