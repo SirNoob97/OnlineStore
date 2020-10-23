505752bda9df8bdfd4350c939f8ceb263a3065e4
Martin <martinald467@gmail.com>
2020-10-23 19:31:03 +0200

    creada clase para validar las constraint(decidi llamarlas asi) definidas con las notaciones de javax validation

---
+++ auth-service/src/main/java/com/sirnoob/authservice/validator/ConstraintValidator.java
@@ -0,0 +1,32 @@
+package com.sirnoob.authservice.validator;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ConstraintValidator {
+
+  private final Validator validator;
+
+  public ConstraintValidator(Validator validator){
+    this.validator = validator;
+  }
+
+  public <T> Mono<T> validateRequest(T t) {
+    Set<ConstraintViolation<T>> constraintViolations = validator.validate(t);
+    String errors = constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
+    if(!errors.isEmpty())
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors);
+
+    return Mono.just(t);
+  }
+}
