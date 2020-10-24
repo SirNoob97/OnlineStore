5f4d1eff1b7c76548286717c51f25f6b2f681441
Martin <martinald467@gmail.com>
2020-10-24 18:42:01 +0200

    agregas notaciones de constructores ya que se necesitan para los test de integracion

--- auth-service/src/main/java/com/sirnoob/authservice/dto/AuthResponse.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/AuthResponse.java
@@ -2,11 +2,15 @@ package com.sirnoob.authservice.dto;
 
 import java.time.Instant;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
 
   private String userName;
