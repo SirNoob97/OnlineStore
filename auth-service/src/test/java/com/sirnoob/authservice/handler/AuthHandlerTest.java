3723359a7984b7db091a70dd5529cc5b79f90a3a
Martin <martinald467@gmail.com>
2020-10-24 20:54:07 +0200

    agregado metodo de test login y removido el mock de jwtProvider ya que necesito authenticacion para los test de refresco de sesion y logout

--- auth-service/src/test/java/com/sirnoob/authservice/handler/AuthHandlerTest.java
+++ auth-service/src/test/java/com/sirnoob/authservice/handler/AuthHandlerTest.java
@@ -1,24 +1,18 @@
 package com.sirnoob.authservice.handler;
 
-import static com.sirnoob.authservice.util.Provider.PASSWORD;
-import static com.sirnoob.authservice.util.Provider.TOKEN;
-import static com.sirnoob.authservice.util.Provider.generateRefreshTokenForIT;
-import static com.sirnoob.authservice.util.Provider.generateSignUpRequest;
-import static com.sirnoob.authservice.util.Provider.generateUserStaticValuesForIT;
-import static com.sirnoob.authservice.util.Provider.getJwtExpirationTime;
+import static com.sirnoob.authservice.util.Provider.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.time.Instant;
-
 import com.sirnoob.authservice.configuration.Router;
 import com.sirnoob.authservice.configuration.SecurityConfig;
 import com.sirnoob.authservice.domain.RefreshToken;
 import com.sirnoob.authservice.domain.User;
 import com.sirnoob.authservice.dto.AuthResponse;
+import com.sirnoob.authservice.dto.LoginRequest;
 import com.sirnoob.authservice.dto.SignUpRequest;
 import com.sirnoob.authservice.repository.IRefreshTokenRepository;
 import com.sirnoob.authservice.repository.IUserRepository;
@@ -30,29 +24,25 @@ import com.sirnoob.authservice.service.AuthServiceImpl;
 import com.sirnoob.authservice.service.RefreshTokenServiceImpl;
 import com.sirnoob.authservice.validator.ConstraintValidator;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import reactor.core.publisher.Mono;
 
-@TestInstance(Lifecycle.PER_CLASS)
 @WebFluxTest
-@Import({ SecurityConfig.class, AuthenticationManager.class, SecurityContextRepository.class, AuthServiceImpl.class,
-    AccountServiceImpl.class, RefreshTokenServiceImpl.class, ConstraintValidator.class })
-@ContextConfiguration(classes = { Router.class, AuthHandler.class, AccountHandler.class })
+@Import({ AuthServiceImpl.class, AccountServiceImpl.class, RefreshTokenServiceImpl.class, JwtProvider.class, ConstraintValidator.class })
+@ContextConfiguration(classes = { SecurityConfig.class, AuthenticationManager.class, SecurityContextRepository.class,
+    Router.class, AuthHandler.class, AccountHandler.class })
 class AuthHandlerTest {
 
   @MockBean
@@ -62,9 +52,6 @@ class AuthHandlerTest {
   private IRefreshTokenRepository iRefreshTokenRepository;
 
   @MockBean
-  private JwtProvider jwtProvider;
-
-  @MockBean
   private PasswordEncoder passwordEncoder;
 
   @Autowired
@@ -72,47 +59,78 @@ class AuthHandlerTest {
 
   private WebTestClient webTestClient;
 
-  @BeforeAll
+  private static final User staticUser = generateUserStaticValuesForIT();
+
+  @BeforeEach
   public void setUp() {
     webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
-    Mono<User> user = Mono.just(generateUserStaticValuesForIT());
+
+    Mono<User> user = Mono.just(staticUser);
 
     Mono<RefreshToken> refreshToken = Mono.just(generateRefreshTokenForIT());
-
-    Instant expirationTime = getJwtExpirationTime();
 
     BDDMockito.when(iUserRepository.save(any(User.class))).thenReturn(user);
 
     BDDMockito.when(iRefreshTokenRepository.save(any(RefreshToken.class))).thenReturn(refreshToken);
 
-    BDDMockito.when(jwtProvider.generateToken(any(User.class))).thenReturn(TOKEN);
-
-    BDDMockito.when(jwtProvider.getJwtExpirationTime()).thenReturn(expirationTime);
-
     BDDMockito.when(passwordEncoder.encode(anyString())).thenReturn(PASSWORD);
+
+    BDDMockito.when(iUserRepository.findByUserName(anyString())).thenReturn(user);
+
+    BDDMockito.when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
+
+    BDDMockito.when(iRefreshTokenRepository.findByToken(anyString())).thenReturn(refreshToken);
   }
 
   @Test
-  @DisplayName("signup return an AuthResponse and 201 status when successful")
-  public void signup_ReturnAAuthResponseAnd201Status_WhenSuccesful() {
+  @DisplayName("signup return an AuthResponse and 201 status code when successful")
+  public void signup_ReturnAAuthResponseAnd201StatusCode_WhenSuccesful() {
     SignUpRequest signUpRequest = generateSignUpRequest();
 
     webTestClient.post()
                   .uri("/auth/signup")
-                  .contentType(MediaType.APPLICATION_JSON)
+                  .contentType(JSON)
+                  .accept(JSON)
                   .body(Mono.just(signUpRequest), SignUpRequest.class)
                   .exchange()
                   .expectStatus().isCreated()
+                  .expectHeader().contentType(JSON)
                   .expectBody(AuthResponse.class)
                   .value(authResponse -> {
                     assertThat(authResponse).isNotNull();
+                    assertThat(authResponse.getAuthToken()).isNotNull();
                     assertThat(authResponse.getUserName()).isEqualTo(signUpRequest.getUserName());
+                    assertThat(authResponse.getUserName()).isEqualTo(staticUser.getUsername());
                   });
 
     verify(iUserRepository, times(1)).save(any(User.class));
     verify(iRefreshTokenRepository, times(1)).save(any(RefreshToken.class));
-    verify(jwtProvider, times(1)).generateToken(any(User.class));
-    verify(jwtProvider, times(1)).getJwtExpirationTime();
     verify(passwordEncoder, times(1)).encode(anyString());
+  }
+
+  @Test
+  @DisplayName("login return AuthResponseand 200 status code when successful")
+  public void login_ReturnAuthResponseAnd200StatusCode_WhenSuccessful() {
+    LoginRequest loginRequest = generateLoginRequest();
+
+    webTestClient.post()
+                  .uri("/auth/login")
+                  .contentType(JSON)
+                  .accept(JSON)
+                  .body(Mono.just(loginRequest), LoginRequest.class)
+                  .exchange()
+                  .expectStatus().isOk()
+                  .expectHeader().contentType(JSON)
+                  .expectBody(AuthResponse.class)
+                  .value(authResponse -> {
+                    assertThat(authResponse).isNotNull();
+                    assertThat(authResponse.getAuthToken()).isNotNull();
+                    assertThat(authResponse.getUserName()).isEqualTo(loginRequest.getUserName());
+                    assertThat(authResponse.getUserName()).isEqualTo(staticUser.getUsername());
+                  });
+
+    verify(iRefreshTokenRepository, times(1)).save(any(RefreshToken.class));
+    verify(iUserRepository, times(1)).findByUserName(anyString());
+    verify(passwordEncoder, times(1)).matches(anyString(), anyString());
   }
 }
