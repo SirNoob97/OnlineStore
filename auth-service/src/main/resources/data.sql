e59e5acefbacd2dbd688d3aca71762cb6759479b
Martin <martinald467@gmail.com>
2020-10-25 18:13:49 +0100

    agregado usuario test y martin para tener un usuario de cada rol

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,2 +1,3 @@
+INSERT INTO users (user_name, password, email, role) VALUES ('martin','$2a$10$Skf1kNT607efc5W4duBvhu3XHKmkgRgP1LpsojsSm7/veunSY1GDW', 'martin@email.com', 'CUSTOMER');
 INSERT INTO users (user_name, password, email, role) VALUES ('employee','$2a$10$ohLOHQxl6i9zhwHwgQI9U.qpRihIdQaBANWFqLV2cS67uHiwX5Kp.', 'useremail@mail.com', 'EMPLOYEE');
-INSERT INTO users (user_name, password, email, role) VALUES ('maldosd','$2a$10$KtgoKvlpJZj3H2stcz8hxOn9VsD0B0ETwwmVK6NbEL1ISrcpZw7q2', 'myawesome@email.com', 'ADMIN');
+INSERT INTO users (user_name, password, email, role) VALUES ('test','$2a$10$Nku2UGt.uENIURSUl/3WHOZe8Sekf2jxoJiqZsjPNFZuJZCedmOsS', 'test@email.com', 'ADMIN');
