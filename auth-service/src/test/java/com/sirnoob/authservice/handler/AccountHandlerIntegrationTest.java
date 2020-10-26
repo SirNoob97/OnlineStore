929c7104e290a4ee951edb8c229145d0baceee0d
Martin <martinald467@gmail.com>
2020-10-26 20:09:45 +0100

    agregados metodos y encontrado cual era la falla que me impedia testear de manera exitosa los metodos de persistencia ->
    en resumen jackso o spring o webflux no se como verificar esto convierte el role del usuario que envio para persistir a una autoridad no un role lo que provoca que no se comprenda la request realizada y se retorne un 400 bad request, ahora con un dto solo para esto que envia un string para ser mapeado en el servicio de los usuarios a un role solucione el problema, esto solo me paso con WebTestClient, nunca con postman

--- auth-service/src/test/java/com/sirnoob/authservice/handler/AccountHandlerIntegrationTest.java
+++ auth-service/src/test/java/com/sirnoob/authservice/handler/AccountHandlerIntegrationTest.java
@@ -2,12 +2,15 @@ package com.sirnoob.authservice.handler;
 
 import static com.sirnoob.authservice.util.Provider.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 
 import com.sirnoob.authservice.configuration.Router;
 import com.sirnoob.authservice.configuration.SecurityConfig;
 import com.sirnoob.authservice.domain.Role;
 import com.sirnoob.authservice.domain.User;
+import com.sirnoob.authservice.dto.AccountPayload;
+import com.sirnoob.authservice.dto.PasswordUpdateDto;
 import com.sirnoob.authservice.repository.IRefreshTokenRepository;
 import com.sirnoob.authservice.repository.IUserRepository;
 import com.sirnoob.authservice.security.AuthenticationManager;
@@ -19,6 +22,7 @@ import com.sirnoob.authservice.service.RefreshTokenServiceImpl;
 import com.sirnoob.authservice.validator.ConstraintValidator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +60,8 @@ class AccountHandlerIntegrationTest{
 
   private static final String ADMIN = "ADMIN";
   private static final User staticUser = User.builder().userName(NEW_USER).password(PASSWORD).email(TEST_EMAIL).role(Role.EMPLOYEE).build();
+  private static final PasswordUpdateDto staticPasswordUpdateDto = new PasswordUpdateDto(1L, PASSWORD);
+  private static final AccountPayload staticAccountPayload = AccountPayload.builder().userName(NEW_USER).password(PASSWORD).email(TEST_EMAIL).role("EMPLOYEE").build();
 
   @BeforeEach
   public void  setUp() {
@@ -70,31 +76,131 @@ class AccountHandlerIntegrationTest{
     BDDMockito.when(iUserRepository.save(any(User.class))).thenReturn(user);
 
     BDDMockito.when(iUserRepository.findAll()).thenReturn(Flux.just(staticUser));
-  }
 
-//pendiente el test de persistir usuarios --Error 400 bad request Failed to read HTTP message
-  //@Test
-  //@WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
-  //public void createAccount_Return201StatusCodeAndAConfirmationMessage_WhenSuccesful(){
-    //webTestClient.post()
-                  //.uri("/accounts")
-                  //.contentType(JSON)
-                  //.accept(JSON)
-                  //.body(Mono.just(staticUser), User.class)
-                  //.exchange()
-                  //.expectStatus().isCreated()
-                  //.expectHeader().contentType(JSON)
-                  //.expectBody(String.class);
-  //}
+    BDDMockito.when(iUserRepository.deleteByUserId(anyLong())).thenReturn(Mono.just(1));
+
+    BDDMockito.when(iUserRepository.updatePasswordById(anyLong(), anyString())).thenReturn(Mono.just(1));
+  }
 
   @Test
   @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
-  public void get() {
+  @DisplayName("createAccount return 201 status code and a confirmation message when succesful")
+  public void createAccount_Return201StatusCodeAndAConfirmationMessage_WhenSuccesful(){
+    webTestClient.post()
+                  .uri("/accounts")
+                  .contentType(JSON)
+                  .accept(JSON)
+                  .body(Mono.just(staticAccountPayload), User.class)
+                  .exchange()
+                  .expectStatus().isCreated()
+                  .expectHeader().contentType(JSON);
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("updatePassword return 204 status code when successful")
+  public void updatePassword_Return204StatusCode_WhenSuccessful() {
+    webTestClient.put()
+                  .uri("/accounts/passwords")
+                  .contentType(JSON)
+                  .body(Mono.just(staticPasswordUpdateDto), PasswordUpdateDto.class)
+                  .exchange()
+                  .expectStatus().isNoContent();
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("updatePassword return 404 status code when repository update operation return 0")
+  public void updatePassword_Return404StatusCode_WhenRepositoryUpdateOperationReturnZero() {
+    BDDMockito.when(iUserRepository.updatePasswordById(anyLong(), anyString())).thenReturn(Mono.just(0));
+
+    webTestClient.put()
+                  .uri("/accounts/passwords")
+                  .contentType(JSON)
+                  .body(Mono.just(staticPasswordUpdateDto), PasswordUpdateDto.class)
+                  .exchange()
+                  .expectStatus().isNotFound();
+  }
+
+  @Test
+  @DisplayName("updatePassword return 403 status code when access is denied")
+  public void updatePassword_Return403StatusCode_WhenAccessIsDenied() {
+    BDDMockito.when(iUserRepository.updatePasswordById(anyLong(), anyString())).thenReturn(Mono.just(0));
+
+    webTestClient.put()
+                  .uri("/accounts/passwords")
+                  .contentType(JSON)
+                  .body(Mono.just(staticPasswordUpdateDto), PasswordUpdateDto.class)
+                  .exchange()
+                  .expectStatus().isForbidden();
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("deleteByUserId return 204 status code when successful")
+  public void deleteByUserId_Return204StatusCode_WhenSuccessful() {
+    webTestClient.delete()
+                  .uri("/accounts/1")
+                  .exchange()
+                  .expectStatus().isNoContent();
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("deleteByUserId return 404 status code when repository delete operation return 0")
+  public void deleteByUserId_Return404StatusCode_WhenRepositoryDeleteOperationReturnZero() {
+    BDDMockito.when(iUserRepository.deleteByUserId(anyLong())).thenReturn(Mono.just(0));
+
+    webTestClient.delete()
+                  .uri("/accounts/1")
+                  .exchange()
+                  .expectStatus().isNotFound();
+  }
+
+  @Test
+  @DisplayName("getAllAccounts return 403 status code and when access is denied")
+  public void deleteByUserId_Return403StatusCode_WhenAccessIsDenied() {
+    webTestClient.delete()
+                  .uri("/accounts/1")
+                  .exchange()
+                  .expectStatus().isForbidden();
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("getAllAccounts return 200 status code and flux user when successful")
+  public void getAllAccounts_Return200StatusCodeAndFluxUser_WhenSuccesful() {
     webTestClient.get()
                   .uri("/accounts")
                   .accept(JSON)
                   .exchange()
                   .expectHeader().contentType(JSON)
                   .expectStatus().isOk();
+  }
+
+  @Test
+  @WithMockUser(username = TEST, password = TEST, authorities  = ADMIN)
+  @DisplayName("getAllAccounts return 404 status code and when flux of user is empty")
+  public void getAllAccounts_Return404StatusCode_WhenFluxUserIsEmpty() {
+    BDDMockito.when(iUserRepository.findAll()).thenReturn(Flux.empty());
+
+    webTestClient.get()
+                  .uri("/accounts")
+                  .accept(JSON)
+                  .exchange()
+                  .expectHeader().contentType(JSON)
+                  .expectStatus().isNotFound();
+  }
+
+  @Test
+  @DisplayName("getAllAccounts return 403 status code and when access is denied")
+  public void getAllAccounts_Return403StatusCode_WhenAccessIsDenied() {
+    BDDMockito.when(iUserRepository.findAll()).thenReturn(Flux.empty());
+
+    webTestClient.get()
+                  .uri("/accounts")
+                  .accept(JSON)
+                  .exchange()
+                  .expectStatus().isForbidden();
   }
 }
