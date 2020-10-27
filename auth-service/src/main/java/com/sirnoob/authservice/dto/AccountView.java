03c31db8f9b00af07e8ffe8fcfd2cc18ce6a436d
Martin <martinald467@gmail.com>
2020-10-27 20:34:26 +0100

    cambio de tipo para los roles esto es un dto asi que no necesito ese enum y eliminado metodos de equals y hashcode estos solo los usaba para un test y no me parece tan necesario solo por 1

--- auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/AccountView.java
@@ -1,15 +1,15 @@
 package com.sirnoob.authservice.dto;
 
-import com.sirnoob.authservice.domain.Role;
-
 public class AccountView {
 
   private Long id;
   private String name;
   private String email;
-  private Role role;
+  private String role;
 
-  public AccountView(Long id, String name, String email, Role role) {
+  public AccountView() {}
+
+  public AccountView(Long id, String name, String email, String role) {
     this.id = id;
     this.name = name;
     this.email = email;
@@ -28,49 +28,7 @@ public class AccountView {
     return this.email;
   }
 
-  public Role getRole() {
+  public String getRole() {
     return this.role;
   }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    result = prime * result + ((role == null) ? 0 : role.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    AccountView other = (AccountView) obj;
-    if (email == null) {
-      if (other.email != null)
-        return false;
-    } else if (!email.equals(other.email))
-      return false;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    if (name == null) {
-      if (other.name != null)
-        return false;
-    } else if (!name.equals(other.name))
-      return false;
-    if (role != other.role)
-      return false;
-    return true;
-  }
-
-
 }
