3f1ce4c2eb9ca3ae209db89beaaee477e0659850
Martin <martinald467@gmail.com>
2020-11-01 20:49:41 +0100

    preparado todos los nuevos servicios y product-service para el primer testeo de config-service

---
+++ gateway-service/src/main/java/com/sirnoob/gatewayservice/Router.java
@@ -0,0 +1,20 @@
+package com.sirnoob.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Router {
+
+  public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+    return routeLocatorBuilder.routes()
+                                .route(route -> route.path("/auth/**", "/accounts/**")
+                                                      .uri("lb://auth-service")
+                                                      .id("auth-service"))
+                                .route(route -> route.path("/products/**", "/main-categories/**", "/sub-categories/**")
+                                                      .uri("lb://product-service")
+                                                      .id("product-service"))
+                                .build();
+  }
+}
