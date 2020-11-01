363241a4af1e39d79a309d1d50481cae0bd4dda8
Martin <martinald467@gmail.com>
2020-11-01 17:47:15 +0100

    agregados nuevos modulos al pom del padre y agregados los servicios de configuracion centralizada, descubrimiento y gateway

---
+++ registry-service/src/test/java/com/sirnoob/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirnoob.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
