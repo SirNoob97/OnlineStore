833be77571ddc859fc9b982001c334f0bd8d8b9c
Martin <martinald467@gmail.com>
2020-11-05 13:01:29 +0100

    clase para la configuracion de la seguridad, de momento, muy simple ya que solo es de testeo

---
+++ gateway-service/src/main/java/com/sirnoob/gatewayservice/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.sirnoob.gatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+    return serverHttpSecurity.csrf().disable()
+                              .formLogin().disable()
+                              .httpBasic().disable()
+                              .exceptionHandling()
+                              .authenticationEntryPoint(
+                                  (swe, authex) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)))
+                              .accessDeniedHandler(
+                                  (swe, accdex) -> Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)))
+                              .and()
+                              .authorizeExchange()
+                              .pathMatchers("/auth/**", "/accounts/**").permitAll()
+                              .and()
+                              .build();
+  }
+}
