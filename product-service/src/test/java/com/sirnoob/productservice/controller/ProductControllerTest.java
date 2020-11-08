62b435b021f6416a3b0f90036ac704e9e33a66cd
Martin <martinald467@gmail.com>
2020-11-08 19:42:41 +0100

    re echos todos los test para usar MockMvc y cambio de foco a integracion

--- product-service/src/test/java/com/sirnoob/productservice/controller/ProductControllerTest.java
+++ product-service/src/test/java/com/sirnoob/productservice/controller/ProductControllerTest.java
@@ -1,239 +1,202 @@
 package com.sirnoob.productservice.controller;
 
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createMainCategoryStaticValues;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductInvoiceResponse;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductListViewStaticValues;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductRequest;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductResponseStaticValues;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductResponseStaticValuesForUpdateTest;
-import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductViewStaticValues;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sirnoob.productservice.dto.ProductInvoiceResponse;
 import com.sirnoob.productservice.dto.ProductListView;
-import com.sirnoob.productservice.dto.ProductRequest;
-import com.sirnoob.productservice.dto.ProductResponse;
-import com.sirnoob.productservice.dto.ProductView;
 import com.sirnoob.productservice.entity.MainCategory;
-import com.sirnoob.productservice.service.IMainCategoryService;
-import com.sirnoob.productservice.service.IProductService;
+import com.sirnoob.productservice.entity.Product;
+import com.sirnoob.productservice.entity.SubCategory;
+import com.sirnoob.productservice.mapper.ProductMapperImpl;
+import com.sirnoob.productservice.mapper.SubCategoryMapperImpl;
+import com.sirnoob.productservice.repository.IMainCategoryRepository;
+import com.sirnoob.productservice.repository.IProductRepository;
+import com.sirnoob.productservice.repository.ISubCategoryRepository;
+import com.sirnoob.productservice.service.MainCategoryServiceImpl;
+import com.sirnoob.productservice.service.ProductServiceImpl;
+import com.sirnoob.productservice.service.SubCategoryServiceImpl;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
+@WebMvcTest
+@Import({ ProductServiceImpl.class, ProductMapperImpl.class, MainCategoryServiceImpl.class, SubCategoryServiceImpl.class, SubCategoryMapperImpl.class })
 @DisplayName("Product Controller Test")
 class ProductControllerTest {
 
-  @InjectMocks
-  private ProductController productController;
+  @MockBean
+  private IMainCategoryRepository iMainCategoryRepository;
 
-  @Mock
-  private IProductService iProductService;
+  @MockBean
+  private ISubCategoryRepository iSubCategoryRespository;
 
-  @Mock
-  private IMainCategoryService iMainCategoryService;
+  @MockBean
+  private IProductRepository iProductRepository;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  private static final MediaType JSON = MediaType.APPLICATION_JSON;
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final String PRODUCT_ID = "$.productId";
+  private static final String PRODUCT_BAR_CODE = "$.productBarCode";
 
   @BeforeEach
   public void setUp() {
-    MainCategory mainCategory = createMainCategoryStaticValues();
 
-    ProductResponse product = createProductResponseStaticValues();
+    Optional<MainCategory> mainCategory = Optional.of(createMainCategory());
 
-    ProductResponse productUpdated = createProductResponseStaticValuesForUpdateTest();
+    Optional<SubCategory> subCategory = Optional.of(createSubCategory());
 
-    PageImpl<ProductListView> productPage = new PageImpl<>(List.of(createProductListViewStaticValues()));
+    Product product = createProductWithMainCategoryAndSubCategory();
 
-    ProductView productView = createProductViewStaticValues();
+    ProductInvoiceResponse productInvoiceResponse = createProductInvoiceResponse();
 
-    BDDMockito.when(iMainCategoryService.getMainCategoryByName(ArgumentMatchers.anyString())).thenReturn(mainCategory);
+    List<ProductListView> listOfProducts = List.of(createProductListViewStaticValues());
 
-    BDDMockito.when(iProductService.createProduct(any(ProductRequest.class), any(MainCategory.class)))
-        .thenReturn(product);
+    PageImpl<ProductListView> pageOfProducts = new PageImpl<>(listOfProducts);
 
-    BDDMockito.when(iProductService.updateProduct(anyLong(), any(ProductRequest.class), any(MainCategory.class)))
-        .thenReturn(productUpdated);
+    BDDMockito.when(iMainCategoryRepository.findByMainCategoryName(anyString())).thenReturn(mainCategory);
 
-    BDDMockito.doNothing().when(iProductService).updateProductStock(anyLong(), anyInt());
+    BDDMockito.when(iSubCategoryRespository.findBySubCategoryName(anyString())).thenReturn(subCategory);
 
-    BDDMockito.when(iProductService.getProductResponseByBarCodeOrProductName(anyLong(), anyString()))
-        .thenReturn(product);
+    BDDMockito.when(iProductRepository.save(any(Product.class))).thenReturn(product);
 
-    BDDMockito.when(iProductService.getProductForInvoiceResponse(anyLong(), anyString()))
-        .thenReturn(createProductInvoiceResponse());
+    BDDMockito.when(iProductRepository.findById(anyLong())).thenReturn(Optional.of(product));
 
-    BDDMockito.doNothing().when(iProductService).deleteProductById(anyLong());
+    BDDMockito.when(iProductRepository.updateProductStockByProductBarCode(anyInt(), anyLong())).thenReturn(1);
 
-    BDDMockito.when(iProductService.getPageOfProductListView(any(Pageable.class))).thenReturn(productPage);
+    BDDMockito.when((iProductRepository.findByProductBarCodeOrProductName(anyLong(), anyString()))).thenReturn(Optional.of(product));
 
-    BDDMockito.when(iProductService.getPageOfProductListViewByMainCategory(anyLong(), any(Pageable.class)))
-        .thenReturn(productPage);
+    BDDMockito.when(iProductRepository.findProductForInvoice(anyLong(), anyString())).thenReturn(Optional.of(productInvoiceResponse));
 
-    BDDMockito.when(iProductService.getPageOfProductListViewByName(anyString(), any(Pageable.class)))
-        .thenReturn(productPage);
+    BDDMockito.doNothing().when(iProductRepository).delete(any(Product.class));
 
-    BDDMockito.when(iProductService.getSetOfProductListViewBySubCategory(any())).thenReturn(productPage.toSet());
+    BDDMockito.when(iProductRepository.getAll(any(PageRequest.class))).thenReturn(pageOfProducts);
 
-    BDDMockito.when(iProductService.getProductViewByName(anyString())).thenReturn(productView);
+    BDDMockito.when(iProductRepository.findByMainCategoryMainCategoryId(anyLong(), any(PageRequest.class))).thenReturn(pageOfProducts);
 
-    BDDMockito.when(iProductService.getPageOfProductListViewByName(anyString(), any(Pageable.class)))
-        .thenReturn(productPage);
+    BDDMockito.when(iSubCategoryRespository.findBySubCategoryName(anyString())).thenReturn(subCategory);
+
+    BDDMockito.when(iProductRepository.findBySubCategory(any(SubCategory.class))).thenReturn(listOfProducts);
+
+    BDDMockito.when(iProductRepository.findByProductName(anyString())).thenReturn(Optional.of(product));
+
+    BDDMockito.when(iProductRepository.findByProductNameContainingIgnoreCase(anyString(), any(PageRequest.class))).thenReturn(pageOfProducts);
   }
 
   @Test
   @DisplayName("createProduct return a ProductResponse when successful")
-  public void createProduct_ReturnAProductResponse_WhenSuccessful() {
-    String expectedName = createProductResponseStaticValues().getProductName();
-
-    ResponseEntity<ProductResponse> productResponse = productController.createProduct(createProductRequest());
-
-    assertThat(productResponse).isNotNull();
-    assertThat(productResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-    assertThat(productResponse.getBody()).isNotNull();
-    assertThat(productResponse.getBody().getProductName()).isEqualTo(expectedName);
+  public void createProduct_ReturnAProductResponse_WhenSuccessful() throws Exception{
+    mockMvc.perform(post("/products").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType(JSON))
+            .andExpect(jsonPath(PRODUCT_ID, Matchers.isA(Integer.class)))
+            .andExpect(jsonPath(PRODUCT_BAR_CODE, Matchers.isA(Long.class)));
   }
 
   @Test
   @DisplayName("updateProduct return a ProductResponse when successful")
-  public void updateProductReturnAProductResponseWhenSuccessful() {
-    Long expectedBarCode = createProductResponseStaticValuesForUpdateTest().getProductBarCode();
-    String expectedName = createProductResponseStaticValuesForUpdateTest().getProductName();
-    Integer expectedStock = createProductResponseStaticValuesForUpdateTest().getProductStock();
-
-    ResponseEntity<ProductResponse> productUpdated = productController.updateProduct(1L, createProductRequest());
-
-    assertThat(productUpdated).isNotNull();
-    assertThat(productUpdated.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(productUpdated.getBody()).isNotNull();
-    assertThat(productUpdated.getBody().getProductBarCode()).isEqualTo(expectedBarCode);
-    assertThat(productUpdated.getBody().getProductName()).isEqualTo(expectedName);
-    assertThat(productUpdated.getBody().getProductStock()).isEqualTo(expectedStock);
+  public void updateProductReturnAProductResponseWhenSuccessful() throws Exception{
+    mockMvc.perform(put("/products/1").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON))
+            .andExpect(jsonPath(PRODUCT_ID, Matchers.isA(Integer.class)))
+            .andExpect(jsonPath(PRODUCT_BAR_CODE, Matchers.isA(Long.class)));
   }
 
   @Test
   @DisplayName("updateProductStock update the stock of a product log when successful")
-  public void updateProductStockUpdateTheStockOfAProductLogWhenSuccessful() {
-    ResponseEntity<Void> responseEntity = productController.updateProductStock(1L, 1000);
-
-    assertThat(responseEntity).isNotNull();
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-    assertThat(responseEntity.getBody()).isNull();
+  public void updateProductStockUpdateTheStockOfAProductLogWhenSuccessful() throws Exception{
+    mockMvc.perform(put("/products/1/stock?quantity=100"))
+            .andExpect(status().isNoContent());
   }
 
   @Test
   @DisplayName("getProductResponse return a ProductResponse when successful")
-  public void getProductResponse_ReturnAProductResponse_WhenSuccesful() {
-    String expectedName = createProductResponseStaticValues().getProductName();
-    ResponseEntity<ProductResponse> productResponse = productController.getProductResponse(0L, expectedName);
-
-    ResponseEntity<ProductResponse> productResponse2 = productController.getProductResponse(1L, "");
-
-    assertThat(productResponse).isNotNull();
-    assertThat(productResponse2).isNotNull();
-    assertThat(productResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(productResponse2.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(productResponse.getBody()).isNotNull();
-    assertThat(productResponse2.getBody()).isNotNull();
-    assertThat(productResponse.getBody().getProductName()).isEqualTo(expectedName);
-    assertThat(productResponse2.getBody().getProductName()).isEqualTo(expectedName);
+  public void getProductResponse_ReturnAProductResponse_WhenSuccesful() throws Exception{
+    mockMvc.perform(get("/products/responses?productBarCode=1&productName=product"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("getProductForInvoice return a ProductInvoiceResponse when successful")
-  public void getProductForInvoice_ResturnAProductInvoiceResponse_WhenSuccessful() {
-    ResponseEntity<ProductInvoiceResponse> productInvoiceResponse = productController.getProductForInvoice(1L, "Name");
-
-    assertThat(productInvoiceResponse).isNotNull();
-    assertThat(productInvoiceResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(productInvoiceResponse.getBody()).isNotNull();
+  public void getProductForInvoice_ResturnAProductInvoiceResponse_WhenSuccessful() throws Exception{
+    mockMvc.perform(get("/products/invoices?productBarCode=1&productName=product"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("deleteProductById delete a product log when successful")
-  public void deleteProductByIdDeleteAProductLogWhenSuccessful() {
-    ResponseEntity<Void> responseEntity = productController.deleteProductById(1L);
-
-    assertThat(responseEntity).isNotNull();
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-    assertThat(responseEntity.getBody()).isNull();
+  public void deleteProductByIdDeleteAProductLogWhenSuccessful() throws Exception{
+    mockMvc.perform(delete("/products/1")).andExpect(status().isNoContent());
   }
 
   @Test
   @DisplayName("listAllProducts return a page of ProductListView when successful")
-  public void listAllProducts_ReturnAPageOfProductListView_WhenSuccessful() {
-    ResponseEntity<Page<ProductListView>> products = productController.listAllProducts(PageRequest.of(0, 10));
-
-    assertThat(products).isNotNull();
-    assertThat(products.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(products.getBody()).isNotNull();
-    assertThat(products.getBody().isEmpty()).isFalse();
+  public void listAllProducts_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+    mockMvc.perform(get("/products?page=0&size=10"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("listProductsByMainCategoryId return a page of ProductListView when successful")
-  public void listProductsByMainCategoryId_ReturnAPageOfProductListView_WhenSuccessful() {
-    ResponseEntity<Page<ProductListView>> products = productController.listProductsByMainCategoryId(1L,
-        PageRequest.of(0, 10));
-
-    assertThat(products).isNotNull();
-    assertThat(products.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(products.getBody()).isNotNull();
-    assertThat(products.getBody().isEmpty()).isFalse();
+  public void listProductsByMainCategoryId_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+    mockMvc.perform(get("/products/main-categories/1?page=0&size=10"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("listProductsBySubCategories return a set of ProductListView when successful")
-  public void listProductsBySubCategories_ReturnASetOfProductListView_WhenSuccessful() {
-    String[] subCategories = { "Sub Category" };
+  public void listProductsBySubCategories_ReturnASetOfProductListView_WhenSuccessful() throws Exception{
+    String[] subCategories = {"SubCategory 1", "SubCategory2"};
 
-    ResponseEntity<Set<ProductListView>> products = productController.listProductsBySubCategories(subCategories);
-
-    assertThat(products).isNotNull();
-    assertThat(products.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(products.getBody()).isNotNull();
-    assertThat(products.getBody().isEmpty()).isFalse();
+    mockMvc.perform(get("/products/sub-categories").contentType(JSON)
+                                                    .content(OBJECT_MAPPER.writeValueAsString(subCategories)))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("getProductByProductName return a ProductView when successful")
-  public void getProductByProductName_ReturnAProductView_WhenSuccessful() {
-    ResponseEntity<ProductView> product = productController.getProductByProductName("name");
-
-    assertThat(product).isNotNull();
-    assertThat(product.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(product.getBody()).isNotNull();
+  public void getProductByProductName_ReturnAProductView_WhenSuccessful() throws Exception{
+    mockMvc.perform(get("/products/names?productName=product"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 
   @Test
   @DisplayName("listByProductNameCoincidences return a page of ProductListView when successful")
-  public void listByProductNameCoincidences_ReturnAPageOfProductListView_WhenSuccessful() {
-    ResponseEntity<Page<ProductListView>> products = productController.listByProductNameCoincidences("sa",
-        PageRequest.of(0, 10));
-
-    assertThat(products).isNotNull();
-    assertThat(products.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(products.getBody()).isNotNull();
-    assertThat(products.getBody().isEmpty()).isFalse();
+  public void listByProductNameCoincidences_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+    mockMvc.perform(get("/products/names/search?productName=product&page=0&size=10"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
   }
 }
