ca35a8b14e744525e2588c6c9099fdfdcf4cfe7a
Martin <martinald467@gmail.com>
2020-11-09 17:51:45 +0100

    agregados test en caso de fallo

--- product-service/src/test/java/com/sirnoob/productservice/controller/ProductControllerTest.java
+++ product-service/src/test/java/com/sirnoob/productservice/controller/ProductControllerTest.java
@@ -1,10 +1,22 @@
 package com.sirnoob.productservice.controller;
 
-import static com.sirnoob.productservice.util.RandomEntityGenerator.*;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createMainCategory;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductInvoiceResponse;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductListViewStaticValues;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductRequest;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createProductWithMainCategoryAndSubCategory;
+import static com.sirnoob.productservice.util.RandomEntityGenerator.createSubCategory;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,8 +49,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 @Import({ ProductServiceImpl.class, ProductMapperImpl.class, MainCategoryServiceImpl.class, SubCategoryServiceImpl.class, SubCategoryMapperImpl.class })
@@ -87,7 +97,7 @@ class ProductControllerTest {
 
     BDDMockito.when(iProductRepository.updateProductStockByProductBarCode(anyInt(), anyLong())).thenReturn(1);
 
-    BDDMockito.when((iProductRepository.findByProductBarCodeOrProductName(anyLong(), anyString()))).thenReturn(Optional.of(product));
+    BDDMockito.when(iProductRepository.findByProductBarCodeOrProductName(anyLong(), anyString())).thenReturn(Optional.of(product));
 
     BDDMockito.when(iProductRepository.findProductForInvoice(anyLong(), anyString())).thenReturn(Optional.of(productInvoiceResponse));
 
@@ -107,8 +117,8 @@ class ProductControllerTest {
   }
 
   @Test
-  @DisplayName("createProduct return a ProductResponse when successful")
-  public void createProduct_ReturnAProductResponse_WhenSuccessful() throws Exception{
+  @DisplayName("createProduct return 201 status code when successful")
+  public void createProduct_Return201StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(post("/products").contentType(JSON)
                                       .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
             .andExpect(status().isCreated())
@@ -118,8 +128,30 @@ class ProductControllerTest {
   }
 
   @Test
-  @DisplayName("updateProduct return a ProductResponse when successful")
-  public void updateProductReturnAProductResponseWhenSuccessful() throws Exception{
+  @DisplayName("createProduct return 404 status code when main category was not found")
+  public void createProduct_Return404StatusCode_WhenMainCategoryWasNotFound() throws Exception{
+    BDDMockito.when(iMainCategoryRepository.findByMainCategoryName(anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(post("/products").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("createProduct return 404 status code when sub category was not found")
+  public void createProduct_Return404StatusCode_WhenSubCategoryWasNotFound() throws Exception{
+    BDDMockito.when(iSubCategoryRespository.findBySubCategoryName(anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(post("/products").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("updateProduct return 200 status code when successful")
+  public void updateProduct_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(put("/products/1").contentType(JSON)
                                       .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
             .andExpect(status().isOk())
@@ -129,53 +161,133 @@ class ProductControllerTest {
   }
 
   @Test
-  @DisplayName("updateProductStock update the stock of a product log when successful")
-  public void updateProductStockUpdateTheStockOfAProductLogWhenSuccessful() throws Exception{
+  @DisplayName("createProduct return 404 status code when main category was not found")
+  public void updateProduct_Return404StatusCode_WhenMainCategoryWasNotFound() throws Exception{
+    BDDMockito.when(iMainCategoryRepository.findByMainCategoryName(anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(put("/products/1").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("createProduct return 404 status code when sub category was not found")
+  public void updateProduct_Return404StatusCode_WhenSubCategoryWasNotFound() throws Exception{
+    BDDMockito.when(iSubCategoryRespository.findBySubCategoryName(anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(put("/products/1").contentType(JSON)
+                                      .content(OBJECT_MAPPER.writeValueAsString(createProductRequest())))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("updateProductStock return 204 status code when successful")
+  public void updateProductStock_Return204StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(put("/products/1/stock?quantity=100"))
             .andExpect(status().isNoContent());
   }
 
   @Test
-  @DisplayName("getProductResponse return a ProductResponse when successful")
-  public void getProductResponse_ReturnAProductResponse_WhenSuccesful() throws Exception{
+  @DisplayName("updateProductStock return 404 status code when update operation returns 0")
+  public void updateProductStock_Return404StatusCode_WhenUpdateOperationsReturnsZero() throws Exception{
+    BDDMockito.when(iProductRepository.updateProductStockByProductBarCode(anyInt(), anyLong())).thenReturn(0);
+
+    mockMvc.perform(put("/products/1/stock?quantity=100"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("getProductResponse return 200 status code when successful")
+  public void getProductResponse_Return200StatusCode_WhenSuccesful() throws Exception{
     mockMvc.perform(get("/products/responses?productBarCode=1&productName=product"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(JSON));
   }
 
   @Test
-  @DisplayName("getProductForInvoice return a ProductInvoiceResponse when successful")
-  public void getProductForInvoice_ResturnAProductInvoiceResponse_WhenSuccessful() throws Exception{
+  @DisplayName("getProductResponse return 404 status code when product was not found")
+  public void getProductResponse_Return404StatusCode_WhenProductWasNotFound() throws Exception{
+    BDDMockito.when(iProductRepository.findByProductBarCodeOrProductName(anyLong(), anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(get("/products/responses?productBarCode=1&productName=product"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("getProductForInvoice return 200 status code when successful")
+  public void getProductForInvoice_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(get("/products/invoices?productBarCode=1&productName=product"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(JSON));
   }
 
   @Test
-  @DisplayName("deleteProductById delete a product log when successful")
-  public void deleteProductByIdDeleteAProductLogWhenSuccessful() throws Exception{
+  @DisplayName("getProductForInvoice return 404 status code when product was not found")
+  public void getProductForInvoice_Return404StatusCode_WhenProductWasNotFound() throws Exception{
+    BDDMockito.when(iProductRepository.findProductForInvoice(anyLong(), anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(get("/products/invoices?productBarCode=1&productName=product"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("deleteProductById return 204 status code when successful")
+  public void deleteProductById_Return204StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(delete("/products/1")).andExpect(status().isNoContent());
   }
 
   @Test
-  @DisplayName("listAllProducts return a page of ProductListView when successful")
-  public void listAllProducts_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+  @DisplayName("deleteProductById return 404 status code when product was not found")
+  public void deleteProductById_Return404StatusCode_WhenProductWasNotFound() throws Exception{
+    BDDMockito.when(iProductRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+    mockMvc.perform(delete("/products/1")).andExpect(status().isNotFound());
+  }
+
+  @Test
+  @DisplayName("listAllProducts return 200 status code when successful")
+  public void listAllProducts_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(get("/products?page=0&size=10"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(JSON));
   }
 
   @Test
-  @DisplayName("listProductsByMainCategoryId return a page of ProductListView when successful")
-  public void listProductsByMainCategoryId_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+  @DisplayName("listAllProducts return 404 status code when no products are found")
+  public void listAllProducts_Return404StatusCode_WhenNoProductsAreFound() throws Exception{
+    BDDMockito.when(iProductRepository.getAll(any(PageRequest.class))).thenReturn(new PageImpl<>(List.of()));
+
+    mockMvc.perform(get("/products?page=0&size=10"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("listProductsByMainCategoryId return 200 status code when successful")
+  public void listProductsByMainCategoryId_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(get("/products/main-categories/1?page=0&size=10"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(JSON));
   }
 
   @Test
-  @DisplayName("listProductsBySubCategories return a set of ProductListView when successful")
-  public void listProductsBySubCategories_ReturnASetOfProductListView_WhenSuccessful() throws Exception{
+  @DisplayName("listProductsByMainCategoryId return 404 status code when no products are found")
+  public void listProductsByMainCategoryId_Return404StatusCode_WhenNoProductsAreFound() throws Exception{
+    BDDMockito.when(iProductRepository.findByMainCategoryMainCategoryId(anyLong(), any(PageRequest.class))).thenReturn(new PageImpl<>(List.of()));
+
+    mockMvc.perform(get("/products/main-categories/1?page=0&size=10"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("listProductsBySubCategories return 200 status code when successful")
+  public void listProductsBySubCategories_Return200StatusCode_WhenSuccessful() throws Exception{
     String[] subCategories = {"SubCategory 1", "SubCategory2"};
 
     mockMvc.perform(get("/products/sub-categories").contentType(JSON)
@@ -185,18 +297,51 @@ class ProductControllerTest {
   }
 
   @Test
-  @DisplayName("getProductByProductName return a ProductView when successful")
-  public void getProductByProductName_ReturnAProductView_WhenSuccessful() throws Exception{
+  @DisplayName("listProductsBySubCategories return 404 status code when that subcategory has no products")
+  public void listProductsBySubCategories_Return404StatusCode_WhenThatSubCategoryHasNoProducts() throws Exception{
+    BDDMockito.when(iProductRepository.findBySubCategory(any(SubCategory.class))).thenReturn(List.of());
+
+    String[] subCategories = {"SubCategory 1", "SubCategory2"};
+
+    mockMvc.perform(get("/products/sub-categories").contentType(JSON)
+                                                    .content(OBJECT_MAPPER.writeValueAsString(subCategories)))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("getProductByProductName return 200 status code when successful")
+  public void getProductByProductName_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(get("/products/names?productName=product"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(JSON));
   }
 
   @Test
-  @DisplayName("listByProductNameCoincidences return a page of ProductListView when successful")
-  public void listByProductNameCoincidences_ReturnAPageOfProductListView_WhenSuccessful() throws Exception{
+  @DisplayName("getProductByProductName return 404 status code when product was not found")
+  public void getProductByProductName_Return404StatusCode_WhenProductWasNotFound() throws Exception{
+    BDDMockito.when(iProductRepository.findByProductName(anyString())).thenReturn(Optional.empty());
+
+    mockMvc.perform(get("/products/names?productName=product"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("listByProductNameCoincidences return 200 status code when successful")
+  public void listByProductNameCoincidences_Return200StatusCode_WhenSuccessful() throws Exception{
     mockMvc.perform(get("/products/names/search?productName=product&page=0&size=10"))
             .andExpect(status().isOk())
+            .andExpect(content().contentType(JSON));
+  }
+
+  @Test
+  @DisplayName("listByProductNameCoincidences return 404 status code when no products are found")
+  public void listByProductNameCoincidences_Return404StatusCode_WhenNoProductsAreFound() throws Exception{
+    BDDMockito.when(iProductRepository.findByProductNameContainingIgnoreCase(anyString(), any(PageRequest.class))).thenReturn(new PageImpl<>(List.of()));
+
+    mockMvc.perform(get("/products/names/search?productName=product&page=0&size=10"))
+            .andExpect(status().isNotFound())
             .andExpect(content().contentType(JSON));
   }
 }
