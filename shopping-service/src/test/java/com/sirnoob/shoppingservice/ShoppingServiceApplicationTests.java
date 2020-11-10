d4b345ff4029a22a25fb97c82c46dd67ba647902
Martin <martinald467@gmail.com>
2020-11-10 17:57:42 +0100

    creado microservicio para la gestion de las ventas

---
+++ shopping-service/src/test/java/com/sirnoob/shoppingservice/ShoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirnoob.shoppingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
