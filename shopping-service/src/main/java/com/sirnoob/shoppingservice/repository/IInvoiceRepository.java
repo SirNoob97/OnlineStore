d6a0dd9417b7a9f8cc6557b2de8bb91ba479cd0d
Martin <martinald467@gmail.com>
2020-11-12 13:01:05 +0100

    creado repo de las facturas

---
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/repository/IInvoiceRepository.java
@@ -0,0 +1,15 @@
+package com.sirnoob.shoppingservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sirnoob.shoppingservice.entity.Invoice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IInvoiceRepository extends JpaRepository<Invoice, Long> {
+
+  public List<Invoice> findByCustomerUserName(String userName);
+
+  public Optional<Invoice> findByNumberInvoice(Long numberInvoice);
+}
