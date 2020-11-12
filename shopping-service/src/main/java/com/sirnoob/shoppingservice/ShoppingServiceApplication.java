3eed566e4de31bda0eeba16b5e48e66b950b1d86
Martin <martinald467@gmail.com>
2020-11-12 20:40:50 +0100

    eliminado testeo y agregadas notaciones necesarias para usar feign y hystrix

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/ShoppingServiceApplication.java
@@ -1,31 +1,18 @@
 package com.sirnoob.shoppingservice;
 
-import com.sirnoob.shoppingservice.entity.Item;
-import com.sirnoob.shoppingservice.model.Product;
-import com.sirnoob.shoppingservice.repository.IItemRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableEurekaClient
 @SpringBootApplication
 public class ShoppingServiceApplication {
-
-  @Autowired
-  private IItemRepository itemRepository;
-
-  @Bean
-  public void test (){
-    Product product = Product.builder().productName("test").productPrice(2.50).build();
-    Item item = Item.builder().quantity(10).product(product).build();
-
-    Item newItem = itemRepository.save(item);
-
-    product.setProductPrice(20.0);
-    newItem.setQuantity(6);
-    itemRepository.save(newItem);
-  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingServiceApplication.class, args);
