72d1845ece1aa47fc715b0770e72fe56c7752b53
Martin <martinald467@gmail.com>
2020-11-12 20:34:50 +0100

    removido el trigger de el sub total no encontre info de este tema con h2 y con las operaciones de actualizar me saltaba un stackoverflow, se llamaba asi mismo

--- shopping-service/src/main/resources/schema.sql
+++ shopping-service/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS invoices (
   invoice_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
-  number_invoice bigint NOT NULL,
+  invoice_number bigint NOT NULL,
   created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   user_name character varying(60) NOT NULL,
   email character varying(60) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS invoices (
   PRIMARY KEY (invoice_id) ,
   CONSTRAINT unique_user_name UNIQUE (user_name),
   CONSTRAINT unique_email UNIQUE (email),
-  CONSTRAINT unique_number_invoice UNIQUE (number_invoice)
+  CONSTRAINT unique_number_invoice UNIQUE (invoice_number)
 );
 
 CREATE TABLE IF NOT EXISTS items (
@@ -18,10 +18,5 @@ CREATE TABLE IF NOT EXISTS items (
   product_price real NOT NULL,
   sub_total real,
   PRIMARY KEY (item_id),
-  CONSTRAINT unique_product_nama UNIQUE (product_name)
+  CONSTRAINT unique_product_name UNIQUE (product_name)
 );
-
-CREATE TRIGGER IF NOT EXISTS calc_sub_total
-AFTER INSERT, UPDATE
-ON items FOR EACH ROW
-CALL "com.sirnoob.shoppingservice.h2.CalculateSubTotal"
