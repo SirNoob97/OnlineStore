8726e4f722da35d4349941e17c81c82d9d057e47
Martin <martinald467@gmail.com>
2020-11-12 13:02:57 +0100

    cambio de la interface Trigger por TriggerAdapter --menos codigo

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/h2/CalculateSubTotal.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/h2/CalculateSubTotal.java
@@ -2,35 +2,26 @@ package com.sirnoob.shoppingservice.h2;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.h2.api.Trigger;
+import org.h2.tools.TriggerAdapter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class CalculateSubTotal implements Trigger{
+public class CalculateSubTotal extends TriggerAdapter{
 
-	@Override
-	public void init(Connection conn, String schemaName, String triggerName, String tableName, boolean before, int type)
-			throws SQLException {
-	}
-
-	@Override
-	public void fire(Connection conn, Object[] oldRow, Object[] newRow) throws SQLException {
-	  final String query = "UPDATE items SET sub_total = items.product_price * items.quantity";
+  @Override
+  public void fire(Connection conn, ResultSet oldRow, ResultSet newRow) throws SQLException {
+    final String query = "UPDATE items SET sub_total = items.product_price * items.quantity";
     try (PreparedStatement statement = conn.prepareStatement(query)) {
       statement.execute();
+
+      statement.close();
+      conn.close();
     } catch (Exception e) {
       new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
-	}
-
-	@Override
-	public void close() throws SQLException {
-	}
-
-	@Override
-	public void remove() throws SQLException {
-	}
+  }
 
 }
