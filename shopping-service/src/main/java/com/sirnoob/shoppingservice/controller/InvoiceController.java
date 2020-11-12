f837131eb9d8f128bda94ea89715d01ed384fd3b
Martin <martinald467@gmail.com>
2020-11-12 20:42:09 +0100

    creado controller de las facturas de momento solo tienen metodo para crear facturas

---
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/controller/InvoiceController.java
@@ -0,0 +1,30 @@
+package com.sirnoob.shoppingservice.controller;
+
+import javax.validation.Valid;
+
+import com.sirnoob.shoppingservice.dto.InvoiceRequest;
+import com.sirnoob.shoppingservice.entity.Invoice;
+import com.sirnoob.shoppingservice.service.IInvoiceService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+  private final IInvoiceService iInvoiceService;
+
+  @PostMapping
+  public ResponseEntity<Invoice> createInvoice(@Valid @RequestBody InvoiceRequest invoiceRequest){
+    return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(iInvoiceService.createInvoice(invoiceRequest));
+  }
+}
