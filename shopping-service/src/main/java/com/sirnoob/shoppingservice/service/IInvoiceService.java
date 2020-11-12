2c5e9a582dea1ad8b5738690e0a0030736984ade
Martin <martinald467@gmail.com>
2020-11-12 20:39:37 +0100

    cambio en los parametros de estos metodos para usar dtos

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/service/IInvoiceService.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/service/IInvoiceService.java
@@ -1,16 +1,17 @@
 package com.sirnoob.shoppingservice.service;
 
+import com.sirnoob.shoppingservice.dto.InvoiceRequest;
 import com.sirnoob.shoppingservice.entity.Invoice;
 
 public interface IInvoiceService {
 
-  public Invoice createInvoice(Invoice invoice);
+  public Invoice createInvoice(InvoiceRequest invoiceRequest);
 
-  public void updateInvoice(Invoice invoice);
+  public void updateInvoice(InvoiceRequest invoiceRequest);
 
   public void deleteInvoice(Long invoiceId);
 
   public Invoice getInvoiceByUserName(String userName);
 
-  public Invoice getInvoiceByNumberInvoice(Long numberInvoice);
+  public Invoice getInvoiceByInvoiceNumber(Long invoiceNumber);
 }
