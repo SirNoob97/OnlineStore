7de1cdbf9ab2e8dfa5816f899a807f71de940761
Martin <martinald467@gmail.com>
2020-11-13 19:49:26 +0100

    implementado los metodos de busqueda por nombre de usuario, eliminacion y busqueda por numero de factura, eliminados los metodos de crear y actualizar ahora solo se usara un metodo para persistir facturas, agregados metodos de apoyo para reducir la verbosidad en los metodos principales

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/service/InvoiceServiceImpl.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/service/InvoiceServiceImpl.java
@@ -10,7 +10,11 @@ import com.sirnoob.shoppingservice.entity.Item;
 import com.sirnoob.shoppingservice.model.Product;
 import com.sirnoob.shoppingservice.repository.IInvoiceRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,49 +22,73 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class InvoiceServiceImpl implements IInvoiceService{
 
+  private static final String INVOICE_NOT_FOUND = "Invoice Not Found!!";
+  private static final String THE_USER_HAS_NO_INVOICES = "The User Has No Invoices!!";
+
   private final IInvoiceRepository iInvoiceRepository;
   private final IProductClient iProductClient;
 
   @Override
-  public Invoice createInvoice(InvoiceRequest invoiceRequest) {
+  public Invoice persistInvoice(InvoiceRequest invoiceRequest) {
     Set<Item> items = invoiceRequest.getProducts()
                                     .stream()
-                                    .map(dtos -> {
-                                      Product product = iProductClient.getProductForInvoice(dtos.getProductBarCode(), dtos.getProductName()).getBody();
-                                      iProductClient.updateProductStock(dtos.getProductBarCode(), (dtos.getQuantity() * -1));
-                                      return Item.builder()
-                                                 .product(product)
-                                                 .quantity(dtos.getQuantity())
-                                                 .subTotal(dtos.getQuantity() * product.getProductPrice())
-                                                 .build();
+                                    .map(productDto -> {
+                                      Product product = iProductClient.getProductForInvoice(productDto.getProductBarCode(), productDto.getProductName()).getBody();
+                                      iProductClient.updateProductStock(productDto.getProductBarCode(), (productDto.getQuantity() * -1));
+                                      return buildItem(product, productDto.getQuantity());
                                     })
                                     .collect(Collectors.toSet());
 
-    Double total = items.stream().map(Item::getSubTotal).reduce(0.0, (a, b) -> a + b);
-
-    Invoice invoice = Invoice.builder().invoiceNumber(invoiceRequest.getInvoiceNumber())
-        .customer(invoiceRequest.getCustomer()).items(items).total(total).build();
-
-    return iInvoiceRepository.save(invoice);
-  }
-
-  @Override
-  public void updateInvoice(InvoiceRequest invoice) {
-
+    return iInvoiceRepository.save(buildInvoice(invoiceRequest, items));
   }
 
   @Override
   public void deleteInvoice(Long invoiceId) {
-
+    Invoice invoice = iInvoiceRepository.findById(invoiceId).orElseThrow(() -> getResponseStatusException(INVOICE_NOT_FOUND));
+    iInvoiceRepository.delete(invoice);
   }
 
   @Override
-  public Invoice getInvoiceByUserName(String userName) {
-    return null;
+  public Page<Invoice> getInvoiceByUserName(String userName, Pageable pageable) {
+    Page<Invoice> invoices = iInvoiceRepository.findByCustomerUserName(userName, pageable);
+    return throwExceptionIfPageIsEmpty(invoices, THE_USER_HAS_NO_INVOICES);
   }
 
   @Override
   public Invoice getInvoiceByInvoiceNumber(Long invoiceNumber) {
-    return null;
+    return iInvoiceRepository.findByInvoiceNumber(invoiceNumber).orElseThrow(() -> getResponseStatusException(INVOICE_NOT_FOUND));
   }
+
+
+
+  private Item buildItem(Product product, Integer quantity){
+    return Item.builder()
+                .product(product)
+                .quantity(quantity)
+                .subTotal(quantity * product.getProductPrice()).build();
+  }
+
+  private Invoice buildInvoice(InvoiceRequest invoiceRequest, Set<Item> items){
+    Double total = items.stream()
+                        .map(Item::getSubTotal)
+                        .reduce(0.0, (a, b) -> a + b);
+
+    return Invoice.builder()
+                  .invoiceId(invoiceRequest.getInvoiceId())
+                  .invoiceNumber(invoiceRequest.getInvoiceNumber())
+                  .customer(invoiceRequest.getCustomer())
+                  .items(items)
+                  .total(total).build();
+  }
+
+  private ResponseStatusException getResponseStatusException(String message){
+    return new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+  }
+
+  private <T> Page<T> throwExceptionIfPageIsEmpty(Page<T> page, String message) {
+    if (!page.isEmpty())
+      return page;
+    throw getResponseStatusException(message);
+  }
+
 }
