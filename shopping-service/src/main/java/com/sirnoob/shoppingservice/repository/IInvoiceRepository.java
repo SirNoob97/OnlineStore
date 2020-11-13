a45d54956962f292f20bfed257ae157e4d1a7861
Martin <martinald467@gmail.com>
2020-11-13 19:46:11 +0100

    ahora este metodo retornara un page en lugar de una lista

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/repository/IInvoiceRepository.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/repository/IInvoiceRepository.java
@@ -1,15 +1,16 @@
 package com.sirnoob.shoppingservice.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import com.sirnoob.shoppingservice.entity.Invoice;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IInvoiceRepository extends JpaRepository<Invoice, Long> {
 
-  public List<Invoice> findByCustomerUserName(String userName);
+  public Page<Invoice> findByCustomerUserName(String userName, Pageable pageable);
 
   public Optional<Invoice> findByInvoiceNumber(Long invoiceNumber);
 }
