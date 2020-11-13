f26a9386ed426b08a1a77d38ef7cf57203648111
Martin <martinald467@gmail.com>
2020-11-13 18:50:26 +0100

    multiplicada la cantidad de productos para que product-service decremente el stock y reformat

--- shopping-service/src/main/java/com/sirnoob/shoppingservice/service/InvoiceServiceImpl.java
+++ shopping-service/src/main/java/com/sirnoob/shoppingservice/service/InvoiceServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import com.sirnoob.shoppingservice.client.IProductClient;
 import com.sirnoob.shoppingservice.dto.InvoiceRequest;
-import com.sirnoob.shoppingservice.dto.ProductDto;
 import com.sirnoob.shoppingservice.entity.Invoice;
 import com.sirnoob.shoppingservice.entity.Item;
 import com.sirnoob.shoppingservice.model.Product;
@@ -14,9 +13,7 @@ import com.sirnoob.shoppingservice.repository.IInvoiceRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class InvoiceServiceImpl implements IInvoiceService{
@@ -26,19 +23,18 @@ public class InvoiceServiceImpl implements IInvoiceService{
 
   @Override
   public Invoice createInvoice(InvoiceRequest invoiceRequest) {
-    log.warn("{}", invoiceRequest.getProducts().stream().map(ProductDto::getProductName).collect(Collectors.joining()));
-
-    Set<Item> items = invoiceRequest.getProducts().stream()
-                                                  .map(dtos -> {
-                                                    Product product = iProductClient.getProductForInvoice(dtos.getProductBarCode(), dtos.getProductName()).getBody();
-                                                    log.warn("{}", product.getProductName());
-                                                    return Item.builder()
-                                                                .product(product)
-                                                                .quantity(dtos.getQuantity())
-                                                                .subTotal(dtos.getQuantity() * product.getProductPrice())
-                                                                .build();
-                                                  })
-                                                  .collect(Collectors.toSet());
+    Set<Item> items = invoiceRequest.getProducts()
+                                    .stream()
+                                    .map(dtos -> {
+                                      Product product = iProductClient.getProductForInvoice(dtos.getProductBarCode(), dtos.getProductName()).getBody();
+                                      iProductClient.updateProductStock(dtos.getProductBarCode(), (dtos.getQuantity() * -1));
+                                      return Item.builder()
+                                                 .product(product)
+                                                 .quantity(dtos.getQuantity())
+                                                 .subTotal(dtos.getQuantity() * product.getProductPrice())
+                                                 .build();
+                                    })
+                                    .collect(Collectors.toSet());
 
     Double total = items.stream().map(Item::getSubTotal).reduce(0.0, (a, b) -> a + b);
 
