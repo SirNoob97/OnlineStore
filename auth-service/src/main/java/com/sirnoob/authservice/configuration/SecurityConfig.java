67b48d36341e360c75e034546c53b13c057db2de
Martin <martinald467@gmail.com>
2020-11-15 21:12:44 +0100

    agregada configuracion de seguridad para que solo usuarios authenticados puedan crear facturas y solo admins y empleados puedan eliminarlas

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/SecurityConfig.java
@@ -5,7 +5,7 @@ import com.sirnoob.authservice.security.AuthenticationManager;
 import com.sirnoob.authservice.security.SecurityContextRepository;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import static org.springframework.http.HttpMethod.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -13,6 +13,7 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -43,20 +44,22 @@ public class SecurityConfig {
                               .authenticationManager(authenticationManager)
                               .securityContextRepository(securityContextRepository)
                               .authorizeExchange()
-                              .pathMatchers(HttpMethod.GET, "/accounts").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.POST, "/accounts").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.PUT, "/accounts", "/accounts/**").authenticated()
-                              .pathMatchers(HttpMethod.DELETE, "/accounts/**").authenticated()
+                              .pathMatchers(GET, "/accounts").hasAuthority(ADMIN)
+                              .pathMatchers(POST, "/accounts").hasAuthority(ADMIN)
+                              .pathMatchers(PUT, "/accounts", "/accounts/**").authenticated()
+                              .pathMatchers(DELETE, "/accounts/**").authenticated()
                               .pathMatchers("/auth/users", "/auth/logout", "/auth/refresh-token").authenticated()
                               .pathMatchers("/auth/login", "/auth/signup").permitAll()
-                              .pathMatchers(HttpMethod.POST, "/products").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.PUT, "/products/**").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.DELETE, "/products/**").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.GET, "/products/responses", "/products/invoices").hasAnyAuthority(ADMIN, EMPLOYEE)
-                              .pathMatchers(HttpMethod.POST, "/main-categories", "/sub-categories").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.PUT, "/main-categories/**", "/sub-categories/**").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.DELETE, "/main-categories/**", "/sub-categories/**").hasAuthority(ADMIN)
-                              .pathMatchers(HttpMethod.GET, "/main-categories/{}", "/sub-categories/{}").hasAnyAuthority(ADMIN, EMPLOYEE)
+                              .pathMatchers(POST, "/products").hasAuthority(ADMIN)
+                              .pathMatchers(PUT, "/products/**").hasAuthority(ADMIN)
+                              .pathMatchers(DELETE, "/products/**").hasAuthority(ADMIN)
+                              .pathMatchers(GET, "/products/responses", "/products/invoices").hasAnyAuthority(ADMIN, EMPLOYEE)
+                              .pathMatchers(POST, "/main-categories", "/sub-categories").hasAuthority(ADMIN)
+                              .pathMatchers(PUT, "/main-categories/**", "/sub-categories/**").hasAuthority(ADMIN)
+                              .pathMatchers(DELETE, "/main-categories/**", "/sub-categories/**").hasAuthority(ADMIN)
+                              .pathMatchers(GET, "/main-categories/{}", "/sub-categories/{}").hasAnyAuthority(ADMIN, EMPLOYEE)
+                              .pathMatchers(POST, "/invoices").authenticated()
+                              .pathMatchers(DELETE, "/invoices/**").hasAnyAuthority(ADMIN, EMPLOYEE)
                               .anyExchange().permitAll()
                               .and()
                               .build();
