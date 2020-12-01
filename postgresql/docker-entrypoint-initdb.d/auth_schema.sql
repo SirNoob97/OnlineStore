f3abaf8a0800726860745b6551c81a5a8ca06635
Martin <martinald467@gmail.com>
2020-12-01 20:11:57 +0100

    created postgres container volume for initialize database

---
+++ postgresql/docker-entrypoint-initdb.d/auth_schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+  id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
+  user_name character varying(60) NOT NULL,
+  password character varying(60) NOT NULL,
+  email character varying(60) NOT NULL,
+  role character varying(10) NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT unique_username UNIQUE (user_name),
+  CONSTRAINT unique_email UNIQUE (email),
+  check (role in ('CUSTOMER', 'EMPLOYEE', 'ADMIN')));
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+  id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
+  token character varying(60) NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT unique_token UNIQUE (token));
---
+++ postgresql/docker-entrypoint-initdb.d/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS invoices (
+  invoice_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
+  invoice_number bigint NOT NULL,
+  created_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_name character varying(60) NOT NULL,
+  user_email character varying(60) NOT NULL,
+  total decimal(10,2) NOT NULL,
+  PRIMARY KEY (invoice_id) ,
+  CONSTRAINT unique_number_invoice UNIQUE (invoice_number)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+  item_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT 1),
+  quantity integer NOT NULL,
+  product_bar_code bigint NOT NULL,
+  product_name character varying(130) NOT NULL,
+  product_price decimal(5,2) NOT NULL,
+  sub_total decimal(7,2) NOT NULL,
+  fk_invoice_id bigint,
+  PRIMARY KEY (item_id)
+);
+
+ALTER TABLE items ADD CONSTRAINT fk_invoices FOREIGN KEY(fk_invoice_id) REFERENCES invoices(invoice_id);
