20a973d16535b1371758730d1cd98d5350aa834f
Martin <martinald467@gmail.com>
2020-12-03 21:11:45 +0100

    add postgres env variables for database connection

--- auth-service/src/main/java/com/sirnoob/authservice/configuration/PostgreSQLConfig.java
+++ auth-service/src/main/java/com/sirnoob/authservice/configuration/PostgreSQLConfig.java
@@ -1,5 +1,6 @@
 package com.sirnoob.authservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,17 +15,31 @@ import io.r2dbc.spi.ConnectionFactoryOptions;
 @EnableR2dbcRepositories
 public class PostgreSQLConfig {
 
+  @Value("${envs.postgres.hostname}")
+  private String hostname;
+
+  @Value("${envs.postgres.port}")
+  private Integer port;
+
+  @Value("${envs.postgres.database}")
+  private String dataBase;
+
+  @Value("${envs.postgres.user}")
+  private String user;
+
+  @Value("${envs.postgres.password}")
+  private String password;
+
   @Bean
   public ConnectionFactory connectionFactory() {
     ConnectionFactoryOptions options = ConnectionFactoryOptions.builder()
         .option(ConnectionFactoryOptions.DRIVER, "postgresql")
-        .option(ConnectionFactoryOptions.HOST, "localhost")
-        .option(ConnectionFactoryOptions.PORT, 5432)
-        .option(ConnectionFactoryOptions.USER, "postgres")
-        .option(ConnectionFactoryOptions.PASSWORD, "postgres")
-        .option(ConnectionFactoryOptions.DATABASE, "store")
+        .option(ConnectionFactoryOptions.HOST, hostname)
+        .option(ConnectionFactoryOptions.PORT, port)
+        .option(ConnectionFactoryOptions.DATABASE, dataBase)
+        .option(ConnectionFactoryOptions.USER, user)
+        .option(ConnectionFactoryOptions.PASSWORD, password)
         .build();
     return ConnectionFactories.get(options);
   }
-
 }
