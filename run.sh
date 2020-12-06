668ad22cb263ec60ced408c362402aac2a499d83
Martin <martinald467@gmail.com>
2020-12-06 21:14:02 +0100

    use getop's' instead getopt

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -u
 
 show_help() {
   echo "$1"
@@ -16,92 +17,80 @@ show_help() {
   -s, -S    Specify shopping-service microservice"
 }
 
-if [[ $# -eq 0 ]]; then
-  show_help "You need to specify the profile that each microservices will use"
-  exit 1
-fi
+validate_profile(){
+  if [[ ! -z $2 ]] && [[ $2 = $TEST || $2 = $POSTGRESQL ]] ; then
+   eval $1=$2
+  else
+    if [[ -z $2 ]]; then
+    show_help "Profiles are required!!";
+    exit 1
+    fi
+    show_help "Invalid Profile \"$2\""
+    exit 1
+  fi
+}
 
-read -p "Will you use the postgres container?
-If you don't want to use postgresql microservices will run with h2 database [y/n]" user_input
+main(){
+  if [[ $# -eq 0 ]]; then
+    show_help "You need to specify the profile that each microservices will use"
+    exit 1
+  fi
 
-if [[ "y" == ${user_input,,} ]]; then
-  AUTH_P=""
-  PRODUCT_P=""
-  SHOPPING_P=""
+  arguments=$@
+  local AUTH_P=""
+  local PRODUCT_P=""
+  local SHOPPING_P=""
 
-  arguments=`getopt -o "a:A:p:P:s:S:" -l "auth-service:,product-service:,shopping-service:" -n microservices -- "$@"`
-  echo $arguments
-  eval set -- "$arguments"
-  while :
-  do
-    case $1 in
-      -a|-A) AUTH_P=$2; echo "a getopts $AUTH_P"; shift 2;;
-      -p|-P) PRODUCT_P=$2; echo "p getopts $PRODUCT_P"; shift 2;;
-      -s|-S) SHOPPING_P=$2; echo "s getopts $SHOPPING_P"; shift 2;;
-      :) show_help "Syntax ERROR; option "\"-$2\"" requires an profile"; exit 1;;
-      --) shift; break ;;
-      *) show_help "Invalid option "\"-$2\"""; exit 1;;
+  local OPS=":a:A:p:P:s:S:-:"
+
+  while getopts $OPS options; do
+    case $options in
+      a|A) validate_profile AUTH_P $OPTARG
+            echo "auth-service will use \"$AUTH_P\" profile"
+            ;;
+      p|P) validate_profile PRODUCT_P $OPTARG
+            echo "product-service will use \"$PRODUCT_P\" profile"
+            ;;
+      s|S) validate_profile SHOPPING_P $OPTARG
+            echo "shopping-service will use \"$SHOPPING_P\" profile"
+            ;;
+      -) case $OPTARG in
+          auth-service) validate_profile AUTH_P ${!OPTIND}
+                        OPTIND=$(( $OPTIND + 1 ))
+                        echo "auth-service will use \"$AUTH_P\" profile"
+                        ;;
+          product-service) validate_profile PRODUCT_P ${!OPTIND}
+                        OPTIND=$(( $OPTIND + 1 ))
+                        echo "product-service will use \"$PRODUCT_P\" profile"
+                        ;;
+          shopping-service) validate_profile SHOPPING_P ${!OPTIND}
+                            OPTIND=$(( $OPTIND + 1 ))
+                            echo "shopping-service will use \"$SHOPPING_P\" profile"
+                            ;;
+          *) show_help "Invalid option \"-$OPTARG\""; exit 1;;
+        esac;;
+      :) show_help "Option \"-$OPTARG\" needs a profile"; exit 1;;
+      *) show_help "Invalid option \"-$OPTARG\""; exit 1;;
     esac
   done
-  #while [ $# -gt 0 ]; do
-    #if [[ $1 =~ ^((-{1})([p|P]$|[a|A]$|[s|S]$)) ]]; then
-      #opt=$1; shift;
-      #current_arg=${1,,}
 
-      #if [[ $current_arg =~ ^-{1,2}.* ]]; then
-        #show_help "You may have left an argument blank. Double check your command"; exit 0
-      #fi
-
-      #if [[ $current_arg == "test" || $current_arg == "postgresql" ]] ; then
-
-        #case $opt in
-          #"-a"|"-A") AUTH_P=$current_arg; echo $AUTH_P; shift;;
-          #"-p"|"-P") PRODUCT_P=$current_arg; echo $PRODUCT_P; shift;;
-          #"-s"|"-S") SHOPPING_P=$current_arg; echo $SHOPPING_P; shift;;
-          #*) echo "Invalid option \""$opt"\"" >&2
-          #exit 1;;
-        #esac
-
-      #else
-        #show_help "Invalid Profile $current_arg"; exit 0
-      #fi
-
-    #else
-      #show_help "Invalid option \"$1\""; exit 0
-      #exit 1
-    #fi
-  #done
-
-  if [[ $PRODUCT_P == "" || $AUTH_P == "" || $SHOPPING_P == "" ]]; then
-    show_help "Profiles are required!!"; exit 0
-    exit 1
-  else
-    echo "postgresql"
-    #sudo docker-compose run -d postgresql
-    echo "config"
-    #sudo docker-compose run -d config-service
-    echo "registry"
-    #sudo docker-compose run -d registry-service
-    echo "auth"
-    #sudo docker-compose run -d  -e AUTH_PROFILE=$AUTH_P auth-service
-    echo "product"
-    #sudo docker-compose run -d  -e PRODUCT_PROFILE=$PRODUCT_P product-service
-    echo "shopping"
-    #sudo docker-compose run -d  -e SHOPPING_PROFILE=$SHOPPING_P shopping-service
-  fi
-else
-  if [[ "n" == ${user_input,,} ]]; then
-    echo "no postgres"
-    echo "config"
-    #sudo docker-compose run -d config-service
-    echo "registry"
-    #sudo docker-compose run -d registry-service
-    echo "auth"
-    #sudo docker-compose run -d  -e AUTH_PROFILE=test auth-service
-    echo "product"
-    #sudo docker-compose run -d  -e PRODUCT_PROFILE=test product-service
-    echo "shopping"
-    #sudo docker-compose run -d  -e SHOPPING_PROFILE=test shopping-service
-  fi
-fi
+    if [[ $arguments[@] =~ $POSTGRESQL ]]; then
+      #echo "postgresql"
+      sudo docker-compose run -d postgresql
+    fi
+    #echo "config"
+    sudo docker-compose run -d config-service
+    #echo "registry"
+    sudo docker-compose run -d registry-service
+    #echo "auth $AUTH_P"
+    sudo docker-compose run -d  -e AUTH_PROFILE=$AUTH_P auth-service
+    #echo "product $PRODUCT_P"
+    sudo docker-compose run -d  -e PRODUCT_PROFILE=$PRODUCT_P product-service
+    #echo "shopping $SHOPPING_P"
+    sudo docker-compose run -d  -e SHOPPING_PROFILE=$SHOPPING_P shopping-service
 exit 0
+}
+
+TEST="test"
+POSTGRESQL="postgresql"
+main $@
