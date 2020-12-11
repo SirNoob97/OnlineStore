e0e52f91e38d640e5386d6714a61c3ed9ed37b2c
Martin <martinald467@gmail.com>
2020-12-11 18:02:15 +0100

    finish auth header config for config-service request

--- product-service/src/main/java/com/sirnoob/productservice/configuration/CustomConfigServerRequest.java
+++ product-service/src/main/java/com/sirnoob/productservice/configuration/CustomConfigServerRequest.java
@@ -1,78 +1,57 @@
 package com.sirnoob.productservice.configuration;
 
-import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 
 import com.sirnoob.productservice.security.JwtProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.config.client.ConfigClientProperties;
 import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@Import({JwtProvider.class})
 @Configuration
-public class CustomConfigServerRequest extends ConfigServicePropertySourceLocator{
+public class CustomConfigServerRequest {
 
-  private final JwtProvider jwtProvider;
-  private final Environment environment;
+  @Autowired
+  private ConfigClientProperties configClientProperties;
+  @Autowired
+  private JwtProvider jwtProvider;
 
   Logger log = LoggerFactory.getLogger(CustomConfigServerRequest.class);
 
-  public CustomConfigServerRequest (ConfigClientProperties clientProperties, Environment environment, JwtProvider jwtProvider){
-    super(clientProperties);
-    this.environment = environment;
-    this.jwtProvider = jwtProvider;
-  }
-
-  @Bean
-  public ConfigClientProperties clientProperties() {
-    ConfigClientProperties client = new ConfigClientProperties(this.environment);
-    client.setEnabled(false);
-    return client;
+  public ConfigClientProperties configClientProperties() {
+    return configClientProperties;
   }
 
   @Bean
   public ConfigServicePropertySourceLocator configServicePropertySourceLocator() {
-    ConfigClientProperties clientProperties = clientProperties();
+    ConfigClientProperties clientProperties = configClientProperties();
+    clientProperties.setUsername("product-service");
     var sourceLocator = new ConfigServicePropertySourceLocator(clientProperties);
-    sourceLocator.setRestTemplate(getCustomRestTemplate(clientProperties));
+    sourceLocator.setRestTemplate(getSecureRestTemplate(clientProperties));
     return sourceLocator;
   }
 
-  private RestTemplate getCustomRestTemplate(ConfigClientProperties clientProperties) {
+  private RestTemplate getSecureRestTemplate(ConfigClientProperties clientProperties) {
     SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-    requestFactory.setReadTimeout(60000);
-
-log.info(jwtProvider.generateToken());
+    requestFactory.setReadTimeout(clientProperties.getRequestReadTimeout());
 
     RestTemplate template = new RestTemplate(requestFactory);
-    template.setInterceptors(Arrays.<ClientHttpRequestInterceptor>asList(new AuthorizationToken(jwtProvider.generateToken())));
+
+    clientProperties.getHeaders().remove(ConfigClientProperties.AUTHORIZATION);
+
+    template.setInterceptors(Arrays.<ClientHttpRequestInterceptor>asList(new ConfigServicePropertySourceLocator.GenericRequestHeaderInterceptor
+            (Map.of("authorization", "Bearer " + jwtProvider.generateToken()))));
 
     return template;
-  }
-
-  private static class AuthorizationToken implements ClientHttpRequestInterceptor {
-
-    private final String authenticationToken;
-
-    public AuthorizationToken(String authenticationToken) {
-      this.authenticationToken = authenticationToken == null ? "" : authenticationToken;
-    }
-
-    @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
-        throws IOException {
-      request.getHeaders().add("Authorization", authenticationToken);
-      return execution.execute(request, body);
-    }
   }
 }
