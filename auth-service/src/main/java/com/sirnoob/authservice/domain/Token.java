0696503ccdd592a1b904055981bad8e0ca90c768
Martin <martinald467@gmail.com>
2021-02-09 21:15:10 +0100

    rename

---
+++ auth-service/src/main/java/com/sirnoob/authservice/domain/Token.java
@@ -0,0 +1,36 @@
+package com.sirnoob.authservice.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Builder;
+
+@Builder
+@Table(value = "refresh_tokens")
+public class Token {
+
+  @Id
+  private Long id;
+  private String refreshToken;
+  private String accessToken;
+
+  public Token(){}
+
+  public Token(Long id, String refreshToken, String accessToken) {
+    this.id = id;
+    this.refreshToken = refreshToken;
+    this.accessToken = accessToken;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getRefreshToken() {
+    return this.refreshToken;
+  }
+
+  public String getAccessToken() {
+    return this.accessToken;
+  }
+}
