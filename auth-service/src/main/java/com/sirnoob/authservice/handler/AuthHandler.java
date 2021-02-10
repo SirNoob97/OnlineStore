334243d58752bec61b14e1188fde897636df471d
Martin <martinald467@gmail.com>
2021-02-10 21:36:31 +0100

    refactor now both tokens will be neccessary for the refresh method

--- auth-service/src/main/java/com/sirnoob/authservice/handler/AuthHandler.java
+++ auth-service/src/main/java/com/sirnoob/authservice/handler/AuthHandler.java
@@ -1,17 +1,19 @@
 package com.sirnoob.authservice.handler;
 
+import com.sirnoob.authservice.domain.Token;
 import com.sirnoob.authservice.dto.AccountView;
 import com.sirnoob.authservice.dto.LoginRequest;
-import com.sirnoob.authservice.dto.RefreshTokenRequest;
 import com.sirnoob.authservice.dto.SignUpRequest;
 import com.sirnoob.authservice.service.IAuthService;
 import com.sirnoob.authservice.service.ITokenService;
 import com.sirnoob.authservice.validator.ConstraintValidator;
 
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -26,7 +28,7 @@ public class AuthHandler {
   private static final String CLEAR_SITE_DATA = "Clear-Site-Data";
   private static final String CLEAR_SITE_DATA_VALUES = "\"cache\", \"cookies\", \"storage\", \"executionContexts\"";
 
-  private final ITokenService iRefreshTokenService;
+  private final ITokenService iTokenService;
   private final IAuthService iAuthService;
   private final ConstraintValidator constraintValidator;
 
@@ -49,27 +51,28 @@ public class AuthHandler {
   }
 
   public Mono<ServerResponse> refreshToken(ServerRequest serverRequest){
-    return getRefreshTokenRequestAndValidate(serverRequest)
+    return getTokens(serverRequest.cookies())
            .flatMap(iAuthService::refreshToken)
            .flatMap(data -> buildServerResponse(HttpStatus.OK, data));
   }
 
   public Mono<ServerResponse> logout(ServerRequest serverRequest){
-    return getRefreshTokenRequestAndValidate(serverRequest)
-           .map(RefreshTokenRequest::getToken)
-           .flatMap(token -> iRefreshTokenService.deleteToken(token))
+    return getTokens(serverRequest.cookies())
+           .map(Token::getRefreshToken)
+           .flatMap(token -> iTokenService.deleteToken(token))
            .then(ServerResponse.noContent().header(CLEAR_SITE_DATA, CLEAR_SITE_DATA_VALUES).build());
   }
 
 
-  private Mono<ServerResponse> buildServerResponse(HttpStatus status, String data){
+  private Mono<ServerResponse> buildServerResponse(HttpStatus status, Token data){
     return ServerResponse.status(status)
-          .cookie(buildCookie("RT", data))
+          .cookie(buildCookie("JWT", data.getAccessToken()))
+          .cookie(buildCookie("RT", data.getRefreshToken()))
           .build();
   }
 
-  private Mono<RefreshTokenRequest> getRefreshTokenRequestAndValidate(ServerRequest serverRequest){
-    return serverRequest.bodyToMono(RefreshTokenRequest.class).doOnNext(constraintValidator::validateRequest);
+  private Mono<Token> getTokens(MultiValueMap<String, HttpCookie> cookies){
+    return Mono.just(Token.builder().refreshToken(cookies.getFirst("RT").getValue()).accessToken(cookies.getFirst("JWT").getValue()).build());
   }
 
   private ResponseCookie buildCookie(String name, String value) {
