348e1aaf881f417c86bfb99d64861dd834f0ea49
Martin <martinald467@gmail.com>
2021-02-10 21:41:06 +0100

    --pending

--- auth-service/src/main/java/com/sirnoob/authservice/dto/AuthResponse.java
+++ auth-service/src/main/java/com/sirnoob/authservice/dto/AuthResponse.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthResponse {
 
+  private String userName;
   private String authToken;
   private String refreshToken;
   private Instant expiresAt;
