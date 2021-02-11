a178dca95e00e49532676764c81b8efc920a1a16
Martin <martinald467@gmail.com>
2021-02-11 21:06:35 +0100

    refactor --pending verify cookies

--- auth-service/src/test/java/com/sirnoob/authservice/handler/AuthHandlerIntegrationTest.java
+++ auth-service/src/test/java/com/sirnoob/authservice/handler/AuthHandlerIntegrationTest.java
@@ -5,7 +5,7 @@ import static com.sirnoob.authservice.util.Provider.JSON;
 import static com.sirnoob.authservice.util.Provider.PASSWORD;
 import static com.sirnoob.authservice.util.Provider.TEST;
 import static com.sirnoob.authservice.util.Provider.TEST_EMAIL;
-import static com.sirnoob.authservice.util.Provider.TOKEN;
+import static com.sirnoob.authservice.util.Provider.generateTokenEntity;
 import static com.sirnoob.authservice.util.Provider.generateLoginRequest;
 import static com.sirnoob.authservice.util.Provider.generateSignUpRequest;
 import static com.sirnoob.authservice.util.Provider.generateUserStaticValuesForIT;
@@ -20,9 +20,7 @@ import com.sirnoob.authservice.configuration.SecurityConfig;
 import com.sirnoob.authservice.domain.Token;
 import com.sirnoob.authservice.domain.User;
 import com.sirnoob.authservice.dto.AccountView;
-import com.sirnoob.authservice.dto.AuthResponse;
 import com.sirnoob.authservice.dto.LoginRequest;
-import com.sirnoob.authservice.dto.RefreshTokenRequest;
 import com.sirnoob.authservice.dto.SignUpRequest;
 import com.sirnoob.authservice.mapper.IUserMapper;
 import com.sirnoob.authservice.repository.ITokenRepository;
@@ -66,7 +64,7 @@ class AuthHandlerIntegrationTest {
   private IUserMapper iUserMapper;
 
   @MockBean
-  private ITokenRepository iRefreshTokenRepository;
+  private ITokenRepository iTokenRepository;
 
   @MockBean
   private PasswordEncoder passwordEncoder;
@@ -78,8 +76,8 @@ class AuthHandlerIntegrationTest {
 
   private static final User staticUser = generateUserStaticValuesForIT();
   private static final LoginRequest staticLoginRequest = generateLoginRequest();
-  private static final RefreshTokenRequest staticRefreshTokenRequest = new RefreshTokenRequest(TOKEN, TEST);
   private static final AccountView staticAccountView = new AccountView(1L, TEST, TEST_EMAIL, ADMIN);
+  private static final Token staticToken = generateTokenEntity();
 
   @BeforeEach
   public void setUp() {
@@ -87,13 +85,13 @@ class AuthHandlerIntegrationTest {
 
     Mono<User> user = Mono.just(staticUser);
 
-    //Mono<Token> refreshToken = Mono.just(generateRefreshTokenForIT());
+    Mono<Token> token = Mono.just(staticToken);
 
     BDDMockito.when(iUserMapper.mapSignUpRequestToUser(any(SignUpRequest.class))).thenReturn(staticUser);
 
     BDDMockito.when(iUserRepository.save(any(User.class))).thenReturn(user);
 
-    //BDDMockito.when(iRefreshTokenRepository.save(any(Token.class))).thenReturn(refreshToken);
+    BDDMockito.when(iTokenRepository.save(any(Token.class))).thenReturn(token);
 
     BDDMockito.when(passwordEncoder.encode(anyString())).thenReturn(PASSWORD);
 
@@ -103,9 +101,9 @@ class AuthHandlerIntegrationTest {
 
     BDDMockito.when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
 
-    //BDDMockito.when(iRefreshTokenRepository.findByRefreshToken(anyString())).thenReturn(refreshToken);
+    BDDMockito.when(iTokenRepository.findByRefreshToken(anyString())).thenReturn(token);
 
-    BDDMockito.when(iRefreshTokenRepository.deleteByRefreshToken(anyString())).thenReturn(Mono.just(1));
+    BDDMockito.when(iTokenRepository.deleteByRefreshToken(anyString())).thenReturn(Mono.just(1));
   }
 
   @Test
@@ -120,17 +118,10 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(signUpRequest), SignUpRequest.class)
                   .exchange()
                   .expectStatus().isCreated()
-                  .expectHeader().contentType(JSON)
-                  .expectBody(AuthResponse.class)
-                  .value(authResponse -> {
-                    assertThat(authResponse).isNotNull();
-                    assertThat(authResponse.getAuthToken()).isNotNull();
-                    assertThat(authResponse.getUserName()).isEqualTo(signUpRequest.getUserName());
-                    assertThat(authResponse.getUserName()).isEqualTo(staticUser.getUsername());
-                  });
+                  .expectBody(Void.class);
 
     verify(iUserRepository, times(1)).save(any(User.class));
-    verify(iRefreshTokenRepository, times(1)).save(any(Token.class));
+    verify(iTokenRepository, times(1)).save(any(Token.class));
     verify(iUserMapper, times(1)).mapSignUpRequestToUser(any(SignUpRequest.class));
   }
 
@@ -146,11 +137,10 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(invalidSignUpRequest), SignUpRequest.class)
                   .exchange()
                   .expectStatus().isBadRequest()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
     verify(iUserRepository, times(0)).save(any(User.class));
-    verify(iRefreshTokenRepository, times(0)).save(any(Token.class));
+    verify(iTokenRepository, times(0)).save(any(Token.class));
     verify(passwordEncoder, times(0)).encode(anyString());
   }
 
@@ -164,16 +154,9 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(staticLoginRequest), LoginRequest.class)
                   .exchange()
                   .expectStatus().isOk()
-                  .expectHeader().contentType(JSON)
-                  .expectBody(AuthResponse.class)
-                  .value(authResponse -> {
-                    assertThat(authResponse).isNotNull();
-                    assertThat(authResponse.getAuthToken()).isNotNull();
-                    assertThat(authResponse.getUserName()).isEqualTo(staticLoginRequest.getUserName());
-                    assertThat(authResponse.getUserName()).isEqualTo(staticUser.getUsername());
-                  });
+                  .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(1)).save(any(Token.class));
+    verify(iTokenRepository, times(1)).save(any(Token.class));
     verify(iUserRepository, times(1)).findByUserName(anyString());
     verify(passwordEncoder, times(1)).matches(anyString(), anyString());
   }
@@ -190,11 +173,10 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(staticLoginRequest), LoginRequest.class)
                   .exchange()
                   .expectStatus().isNotFound()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
     verify(iUserRepository, times(1)).findByUserName(anyString());
-    verify(iRefreshTokenRepository, times(0)).save(any(Token.class));
+    verify(iTokenRepository, times(0)).save(any(Token.class));
     verify(passwordEncoder, times(0)).matches(anyString(), anyString());
   }
 
@@ -210,12 +192,11 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(staticLoginRequest), LoginRequest.class)
                   .exchange()
                   .expectStatus().isBadRequest()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
     verify(iUserRepository, times(1)).findByUserName(anyString());
     verify(passwordEncoder, times(1)).matches(anyString(), anyString());
-    verify(iRefreshTokenRepository, times(0)).save(any(Token.class));
+    verify(iTokenRepository, times(0)).save(any(Token.class));
   }
 
   @Test
@@ -229,12 +210,11 @@ class AuthHandlerIntegrationTest {
                   .body(Mono.just(invalidLoginRequest), LoginRequest.class)
                   .exchange()
                   .expectStatus().isBadRequest()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
     verify(iUserRepository, times(0)).findByUserName(anyString());
     verify(passwordEncoder, times(0)).matches(anyString(), anyString());
-    verify(iRefreshTokenRepository, times(0)).save(any(Token.class));
+    verify(iTokenRepository, times(0)).save(any(Token.class));
   }
 
   @Test
@@ -275,21 +255,13 @@ class AuthHandlerIntegrationTest {
   public void refreshToken_ReturnAuthResponseAnd200StatusCode_WhenSuccessful() {
     webTestClient.post()
                   .uri("/auth/refresh-token")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(staticRefreshTokenRequest), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isOk()
-                  .expectHeader().contentType(JSON)
-                  .expectBody(AuthResponse.class)
-                  .value(authR -> {
-                    assertThat(authR).isNotNull();
-                    assertThat(authR.getUserName()).isNotNull();
-                    assertThat(authR.getUserName()).isEqualTo(staticLoginRequest.getUserName());
-                    assertThat(authR.getAuthToken()).isNotNull();
-                  });
+                  .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(1)).findByRefreshToken(anyString());
+    verify(iTokenRepository, times(1)).findByRefreshToken(anyString());
     verify(iUserRepository, times(1)).findByUserName(anyString());
   }
 
@@ -298,15 +270,14 @@ class AuthHandlerIntegrationTest {
   public void refreshToken_Return403StatusCode_WhenUserIsNotAuthenticated() {
     webTestClient.post()
                   .uri("/auth/refresh-token")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(staticRefreshTokenRequest), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isForbidden()
                   .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(0)).save(any(Token.class));
-    verify(iRefreshTokenRepository, times(0)).findByRefreshToken(anyString());
+    verify(iTokenRepository, times(0)).save(any(Token.class));
+    verify(iTokenRepository, times(0)).findByRefreshToken(anyString());
     verify(iUserRepository, times(0)).findByUserName(anyString());
     verify(passwordEncoder, times(0)).matches(anyString(), anyString());
   }
@@ -317,15 +288,13 @@ class AuthHandlerIntegrationTest {
   public void refreshToken_Return400StatusCode_WhenRefreshTokenRequestHasInvalidFields() {
     webTestClient.post()
                   .uri("/auth/refresh-token")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(new RefreshTokenRequest()), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isBadRequest()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(0)).findByRefreshToken(anyString());
+    verify(iTokenRepository, times(0)).findByRefreshToken(anyString());
     verify(iUserRepository, times(0)).findByUserName(anyString());
   }
 
@@ -335,33 +304,30 @@ class AuthHandlerIntegrationTest {
   public void logout_Return204StatusCode_WhenSuccessful() {
     webTestClient.post()
                   .uri("/auth/logout")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(staticRefreshTokenRequest), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isNoContent()
                   .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(1)).deleteByRefreshToken(anyString());
+    verify(iTokenRepository, times(1)).deleteByRefreshToken(anyString());
   }
 
   @Test
   @WithMockUser(username = TEST, password = TEST, authorities = ADMIN)
   @DisplayName("logout return 404 status code when refresh token was not found")
   public void logout_Return404StatusCode_WhenRefreshTokenWasNotFound() {
-    BDDMockito.when(iRefreshTokenRepository.deleteByRefreshToken(anyString())).thenReturn(Mono.just(0));
+    BDDMockito.when(iTokenRepository.deleteByRefreshToken(anyString())).thenReturn(Mono.just(0));
 
     webTestClient.post()
                   .uri("/auth/logout")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(staticRefreshTokenRequest), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isNotFound()
-                  .expectHeader().contentType(JSON)
                   .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(1)).deleteByRefreshToken(anyString());
+    verify(iTokenRepository, times(1)).deleteByRefreshToken(anyString());
   }
 
   @Test
@@ -369,13 +335,12 @@ class AuthHandlerIntegrationTest {
   public void logout_Return403StatusCode_WhenUserIsNotAuthenticated() {
     webTestClient.post()
                   .uri("/auth/logout")
-                  .contentType(JSON)
-                  .accept(JSON)
-                  .body(Mono.just(staticRefreshTokenRequest), RefreshTokenRequest.class)
+                  .cookie("JWT", staticToken.getAccessToken())
+                  .cookie("RT", staticToken.getRefreshToken())
                   .exchange()
                   .expectStatus().isForbidden()
                   .expectBody(Void.class);
 
-    verify(iRefreshTokenRepository, times(0)).deleteByRefreshToken(anyString());
+    verify(iTokenRepository, times(0)).deleteByRefreshToken(anyString());
   }
 }
