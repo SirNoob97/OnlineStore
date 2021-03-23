57a3eed27cf0bc3fad08797a4cc2809a24bc3aa3
Martin <martinald467@gmail.com>
2021-03-23 17:11:59 +0100

    add resilience4j annotation

--- online-store-backend/shopping-service/src/main/java/com/sirnoob/shoppingservice/client/IProductClient.java
+++ online-store-backend/shopping-service/src/main/java/com/sirnoob/shoppingservice/client/IProductClient.java
@@ -1,5 +1,6 @@
 package com.sirnoob.shoppingservice.client;
 
+import com.sirnoob.shoppingservice.config.FeignConfig;
 import com.sirnoob.shoppingservice.model.Product;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,13 +10,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product-service", fallback = ProductFallback.class)
-public interface IProductClient{
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
+
+
+@FeignClient(name = "product-service", configuration = FeignConfig.class, fallback = ProductFallback.class)
+public interface IProductClient{
+  public final String INVOICES = "invoices";
+  public final String STOCK = "stock";
+
+
+  @CircuitBreaker(name = INVOICES)
   @GetMapping("/products/invoices")
   public ResponseEntity<Product> getProductForInvoice(@RequestParam(required = true) Long productBarCode,
                                                        @RequestParam(required = true) String productName);
 
+  @CircuitBreaker(name = STOCK)
   @PutMapping("/products/{productBarCode}/stock")
   public ResponseEntity<Void> updateProductStock(@PathVariable Long productBarCode, @RequestParam(required = true) Integer quantity);
 }
