70350e0c77a2aa3658f58f1cc0f6cb3ef37c5073
Martin <martinald467@gmail.com>
2021-03-23 19:41:41 +0100

    fix deprecated imports and remove DatabaseClient bean

--- online-store-backend/auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
+++ online-store-backend/auth-service/src/main/java/com/sirnoob/authservice/configuration/H2Config.java
@@ -6,10 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 import io.r2dbc.spi.ConnectionFactories;
@@ -24,11 +23,6 @@ public class H2Config extends AbstractR2dbcConfiguration{
   @Override
   public ConnectionFactory connectionFactory(){
      return ConnectionFactories.get("r2dbc:h2:mem:///accounts?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE");
-  }
-
-  @Bean
-  public DatabaseClient databaseClient(ConnectionFactory connectionFactory){
-    return DatabaseClient.builder().connectionFactory(connectionFactory).build();
   }
 
   @Bean
